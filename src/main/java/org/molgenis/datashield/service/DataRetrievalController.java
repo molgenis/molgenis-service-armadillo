a7d06da43305f63678f0eda0942b83537cf3307b
Fleur Kelpin <f.kelpin@umcg.nl>
2020-02-14 18:22:17 +0100

    feat: connect to controller

--- src/main/java/org/molgenis/datashield/service/DataRetrievalController.java
+++ src/main/java/org/molgenis/datashield/service/DataRetrievalController.java
@@ -1,12 +1,30 @@
 package org.molgenis.datashield.service;
 
+import static java.util.stream.Collectors.toList;
+
+import io.swagger.client.model.Attribute;
+import io.swagger.client.model.AttributeData;
+import io.swagger.client.model.EntityType;
+import java.util.List;
+import java.util.stream.Collectors;
+import org.molgenis.datashield.service.model.Column;
+import org.molgenis.datashield.service.model.Table;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DataRetrievalController {
-  @GetMapping("/retrieve")
-  public String retrieve() {
-    return "test";
+  @Autowired
+  DownloadServiceImpl downloadService;
+
+  @GetMapping("/retrieve/{entityTypeId}")
+  public Resource retrieve(@PathVariable String entityTypeId) {
+    Table table = downloadService.getMetadata(entityTypeId);
+    ResponseEntity<Resource> response = downloadService.download(table);
+    return response.getBody();
   }
 }
---
+++ src/main/java/org/molgenis/datashield/service/model/Table.java
@@ -0,0 +1,24 @@
+package org.molgenis.datashield.service.model;
+import com.google.auto.value.AutoValue;
+import com.google.common.collect.ImmutableList;
+
+@AutoValue
+public abstract class Table {
+  public abstract String name();
+  public abstract ImmutableList<Column> columns();
+
+  public static Builder builder() {
+    return new AutoValue_Table.Builder();
+  }
+
+  @AutoValue.Builder
+  public abstract static class Builder {
+    public abstract Builder setName(String name);
+    abstract ImmutableList.Builder<Column> columnsBuilder();
+    public Builder addColumn(Column column) {
+      columnsBuilder().add(column);
+      return this;
+    }
+    public abstract Table build();
+  }
+}
