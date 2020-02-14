f35bedfc4608216bff8028f614fbd7ae2361d067
tommydeboer <tommydeboer4@gmail.com>
2020-02-14 14:18:58 +0100

    init spring boot project

---
+++ src/main/java/org/molgenis/datashield/service/DatashieldServiceApplication.java
@@ -0,0 +1,13 @@
+package org.molgenis.datashield.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatashieldServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatashieldServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/molgenis/datashield/service/DatashieldServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.molgenis.datashield.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatashieldServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
