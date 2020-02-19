b54b310f55bc771fd657d99acef227af8864636b
tommydeboer <tommydeboer4@gmail.com>
2020-02-19 09:55:24 +0100

    add RExecutorService unit tests

--- src/main/java/org/molgenis/datashield/service/RExecutorServiceImpl.java
+++ src/main/java/org/molgenis/datashield/service/RExecutorServiceImpl.java
@@ -14,6 +14,8 @@ import org.springframework.stereotype.Component;
 @Component
 public class RExecutorServiceImpl implements RExecutorService {
 
+  private static final String R_PACKAGE_REPO_URL = "http://cran.r-project.org";
+
   @Override
   public String execute(String cmd, RConnection connection)
       throws RserveException, REXPMismatchException {
@@ -24,37 +26,45 @@ public class RExecutorServiceImpl implements RExecutorService {
   @Override
   public String assign(InputStream csv, Table table, RConnection connection)
       throws IOException, RserveException {
-    String DATA_FILE_NAME = table.name() + ".csv";
+    String dataFileName = table.name() + ".csv";
+    copyFile(csv, dataFileName, connection);
+    ensurePackage("readr", connection);
+    return assignTable(table, dataFileName, connection);
+  }
 
-    RFileOutputStream outputStream = connection.createFile(DATA_FILE_NAME);
+  private void copyFile(InputStream csv, String dataFileName, RConnection connection)
+      throws IOException {
+    RFileOutputStream outputStream = connection.createFile(dataFileName);
     IOUtils.copy(csv, outputStream);
     csv.close();
     outputStream.close();
-
-    ensurePackage("readr", connection);
-    REXP rexp =
-        connection.eval(
-            String.format(
-                "base::is.null(base::assign('%s', readr::read_csv('%s')))",
-                table.name(), DATA_FILE_NAME));
-
-    connection.eval(String.format("base::unlink('%s')", DATA_FILE_NAME));
-
-    return rexp.toString();
   }
 
   /**
    * Ensure that a R package is installed: check it is available, if not install it.
    *
-   * @param packageName
-   * @return
+   * @param packageName the name of the R package
+   * @throws RserveException when installation fails
    */
-  private REXP ensurePackage(String packageName, RConnection connection) throws RserveException {
-    String repos = "http://cran.r-project.org";
+  @SuppressWarnings("SameParameterValue")
+  private void ensurePackage(String packageName, RConnection connection) throws RserveException {
     String cmd =
         String.format(
             "if (!require(%s)) { install.packages('%s', repos=c('%s'), dependencies=TRUE) }",
-            packageName, packageName, repos);
-    return connection.eval(cmd);
+            packageName, packageName, R_PACKAGE_REPO_URL);
+    connection.eval(cmd);
+  }
+
+  private String assignTable(Table table, String dataFileName, RConnection connection)
+      throws RserveException {
+    REXP rexp =
+        connection.eval(
+            String.format(
+                "base::is.null(base::assign('%s', readr::read_csv('%s')))",
+                table.name(), dataFileName));
+
+    connection.eval(String.format("base::unlink('%s')", dataFileName));
+
+    return rexp.toString();
   }
 }
--- src/test/java/org/molgenis/datashield/service/RExecutorServiceImplTest.java
+++ src/test/java/org/molgenis/datashield/service/RExecutorServiceImplTest.java
@@ -1,17 +1,66 @@
 package org.molgenis.datashield.service;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.junit.jupiter.MockitoExtension;
+import org.molgenis.datashield.service.model.Table;
+import org.rosuda.REngine.REXP;
+import org.rosuda.REngine.REXPMismatchException;
+import org.rosuda.REngine.Rserve.RConnection;
+import org.rosuda.REngine.Rserve.RFileOutputStream;
+import org.rosuda.REngine.Rserve.RserveException;
 
-@ExtendWith(MockitoExtension.class)
 class RExecutorServiceImplTest {
 
-  @Test
-  void execute() {}
+  private RExecutorService executorService;
+
+  @BeforeEach
+  void before() {
+    executorService = new RExecutorServiceImpl();
+  }
 
   @Test
-  void assign() {}
+  void execute() throws RserveException, REXPMismatchException {
+    RConnection rConnection = mock(RConnection.class);
+    REXP rexp = mock(REXP.class);
+    when(rexp.asString()).thenReturn("36.6");
+    when(rConnection.eval("mean(age)")).thenReturn(rexp);
+
+    String result = executorService.execute("mean(age)", rConnection);
+
+    assertEquals("36.6", result);
+  }
+
+  @Test
+  void assign() throws IOException, RserveException {
+    Table table = Table.builder().setName("table_patients").build();
+    RConnection rConnection = mock(RConnection.class);
+    RFileOutputStream rFileOutputStream = mock(RFileOutputStream.class);
+    when(rConnection.createFile("table_patients.csv")).thenReturn(rFileOutputStream);
+    InputStream inputStream = new ByteArrayInputStream("".getBytes());
+    when(rConnection.eval(any())).thenReturn(mock(REXP.class));
+
+    executorService.assign(inputStream, table, rConnection);
+
+    String expectedPackageEval =
+        "if (!require(readr)) { install.packages('readr', repos=c('http://cran.r-project.org'), dependencies=TRUE) }";
+    String expectedAssignEval =
+        "base::is.null(base::assign('table_patients', readr::read_csv('table_patients.csv')))";
+    String expectedUnlinkEval = "base::unlink('table_patients.csv')";
+    assertAll(
+        () -> verify(rConnection).createFile("table_patients.csv"),
+        () -> verify(rFileOutputStream).close(),
+        () -> verify(rConnection).eval(expectedPackageEval),
+        () -> verify(rConnection).eval(expectedAssignEval),
+        () -> verify(rConnection).eval(expectedUnlinkEval));
+  }
 }
