4d50483debfe363e4e4b10ef67767a5e2a78e295
sidohaakma <sido@haakma.org>
2020-02-19 08:56:07 +0100

    chore: fix formatting

--- src/main/java/org/molgenis/datashield/service/DownloadServiceImpl.java
+++ src/main/java/org/molgenis/datashield/service/DownloadServiceImpl.java
@@ -31,11 +31,13 @@ public class DownloadServiceImpl {
 
   /**
    * Retrieves metadata for entity type and maps to datashield {@link Table}.
+   *
    * @param entityTypeId the ID of the entity type, dots will be replaced with underscores
    * @return the {@link Table}
    */
   public Table getMetadata(String entityTypeId) {
-    EntityType entityType = restTemplate.getForObject(METADATA_URL, EntityType.class, normalize(entityTypeId));
+    EntityType entityType =
+        restTemplate.getForObject(METADATA_URL, EntityType.class, normalize(entityTypeId));
     Builder tableBuilder = Table.builder().setName(entityTypeId);
     entityType.getData().getAttributes().getItems().stream()
         .map(Attribute::getData)
@@ -92,12 +94,20 @@ public class DownloadServiceImpl {
    */
   public ResponseEntity<Resource> download(Table table) {
     List<String> columnNames = table.columns().stream().map(Column::name).collect(toList());
-    Map<String, Object> request = Map.of("entityTypeId", normalize(table.name()),
-        "query", Map.of("rules", List.of(List.of())),
-        "attributeNames", columnNames,
-        "colNames", "ATTRIBUTE_NAMES",
-        "entityValues", "ENTITY_IDS",
-        "downloadType", "DOWNLOAD_TYPE_CSV");
+    Map<String, Object> request =
+        Map.of(
+            "entityTypeId",
+            normalize(table.name()),
+            "query",
+            Map.of("rules", List.of(List.of())),
+            "attributeNames",
+            columnNames,
+            "colNames",
+            "ATTRIBUTE_NAMES",
+            "entityValues",
+            "ENTITY_IDS",
+            "downloadType",
+            "DOWNLOAD_TYPE_CSV");
     LinkedMultiValueMap<String, Object> params = new LinkedMultiValueMap<>();
     params.add("dataRequest", request);
     HttpHeaders headers = new HttpHeaders();
