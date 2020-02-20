3b746db2ecde6b12b5186b53b811bf840118a49c
sidohaakma <sido@haakma.org>
2020-02-20 10:20:32 +0100

    tests: added fail tests as well

--- src/test/java/org/molgenis/datashield/r/RDatashieldSessionTest.java
+++ src/test/java/org/molgenis/datashield/r/RDatashieldSessionTest.java
@@ -1,9 +1,6 @@
 package org.molgenis.datashield.r;
 
-import static org.junit.jupiter.api.Assertions.assertAll;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
-
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -13,6 +10,10 @@ import org.rosuda.REngine.REXPMismatchException;
 import org.rosuda.REngine.Rserve.RConnection;
 import org.rosuda.REngine.Rserve.RSession;
 import org.rosuda.REngine.Rserve.RserveException;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class RDatashieldSessionTest {
@@ -24,8 +25,10 @@ class RDatashieldSessionTest {
   @Mock private RConnectionFactory rConnectionFactory;
 
   @BeforeEach
-  public void before() {
+  public void before() throws RserveException
+  {
     this.rDatashieldSession = new RDatashieldSession(rConnectionFactory);
+
   }
 
   @SuppressWarnings("unchecked")
@@ -40,6 +43,38 @@ class RDatashieldSessionTest {
         () -> verify(rConnectionConsumer).accept(rConnection), () -> verify(rConnection).detach());
   }
 
+  @SuppressWarnings("unchecked")
   @Test
-  void sessionCleanup() {}
+  void executeFails() throws REXPMismatchException, RserveException {
+    when(rConnectionFactory.getNewConnection(false)).thenThrow(new RserveException(rConnection, "foutje"));
+
+    Assertions.assertThatThrownBy(() -> rDatashieldSession.execute(rConnectionConsumer)).hasCause(new RserveException(rConnection, "foutje"));
+  }
+
+  @SuppressWarnings("unchecked")
+  @Test
+  void sessionCleanup() throws REXPMismatchException, RserveException
+  {
+    when(rConnectionFactory.getNewConnection(false)).thenReturn(rConnection);
+    when(rConnection.detach()).thenReturn(rSession);
+    when(rSession.attach()).thenReturn(rConnection);
+
+    rDatashieldSession.execute(rConnectionConsumer);
+    rDatashieldSession.sessionCleanup();
+
+    assertAll(() -> verify(rSession).attach(),
+    () -> verify(rConnection).close());
+  }
+
+  @SuppressWarnings("unchecked")
+  @Test
+  void sessionCleanupFails() throws REXPMismatchException, RserveException {
+    when(rConnectionFactory.getNewConnection(false)).thenReturn(rConnection);
+    when(rConnection.detach()).thenReturn(rSession);
+    when(rSession.attach()).thenThrow(new RserveException(rConnection, "foutje"));
+
+    rDatashieldSession.execute(rConnectionConsumer);
+
+    Assertions.assertThatThrownBy(() -> rDatashieldSession.sessionCleanup()).hasCause(new RserveException(rConnection, "foutje"));
+  }
 }
