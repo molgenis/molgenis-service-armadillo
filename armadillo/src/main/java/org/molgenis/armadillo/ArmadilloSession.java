8e0bac93b5f201e19085b5dd3d4edabc4999fe33
Fleur Kelpin <f.kelpin@umcg.nl>
2020-07-23 17:46:43 +0200

    feat(ArmadilloSession): debugging and retry on connection detach

--- armadillo/src/main/java/org/molgenis/armadillo/ArmadilloSession.java
+++ armadillo/src/main/java/org/molgenis/armadillo/ArmadilloSession.java
@@ -1,9 +1,11 @@
 package org.molgenis.armadillo;
 
+import static java.lang.System.exit;
 import static java.util.Objects.requireNonNull;
 
 import java.util.function.Function;
 import javax.annotation.PreDestroy;
+import javax.validation.constraints.NotNull;
 import org.molgenis.armadillo.exceptions.ArmadilloSessionException;
 import org.molgenis.armadillo.service.ArmadilloConnectionFactory;
 import org.rosuda.REngine.Rserve.RConnection;
@@ -11,6 +13,9 @@ import org.rosuda.REngine.Rserve.RSession;
 import org.rosuda.REngine.Rserve.RserveException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.retry.backoff.FixedBackOffPolicy;
+import org.springframework.retry.policy.SimpleRetryPolicy;
+import org.springframework.retry.support.RetryTemplate;
 
 public class ArmadilloSession {
   private RSession rSession = null;
@@ -18,9 +23,18 @@ public class ArmadilloSession {
   private static final Logger logger = LoggerFactory.getLogger(ArmadilloSession.class);
 
   private final ArmadilloConnectionFactory connectionFactory;
+  private final RetryTemplate retryTemplate;
 
   public ArmadilloSession(ArmadilloConnectionFactory connectionFactory) {
     this.connectionFactory = requireNonNull(connectionFactory);
+    retryTemplate = new RetryTemplate();
+    retryTemplate.setThrowLastExceptionOnExhausted(true);
+
+    var fixedBackOffPolicy = new FixedBackOffPolicy();
+    fixedBackOffPolicy.setBackOffPeriod(1000);
+    retryTemplate.setBackOffPolicy(fixedBackOffPolicy);
+    var retryPolicy = new SimpleRetryPolicy(3);
+    retryTemplate.setRetryPolicy(retryPolicy);
   }
 
   public synchronized <T> T execute(Function<RConnection, T> consumer) {
@@ -28,17 +42,29 @@ public class ArmadilloSession {
     try {
       return consumer.apply(connection);
     } finally {
-      tryDetachRConnection(connection);
+      retryDetachRConnection(connection);
     }
   }
 
-  void tryDetachRConnection(RConnection connection) {
+  void retryDetachRConnection(@NotNull RConnection connection) {
     try {
-      rSession = connection.detach();
+      retryTemplate.execute(context -> {
+        try {
+          tryDetachRConnection(connection);
+        } catch (RserveException ex) {
+          logger.warn("Failed to detach connection. retryCount={}", context.getRetryCount());
+          throw ex;
+        }
+        return null;
+      });
     } catch (RserveException e) {
-      logger.error("Failed to detach connection", e);
-      rSession = null;
+      logger.error("Failed to detach connection after retrying", e);
+      connection.close();
     }
+  }
+
+  void tryDetachRConnection(RConnection connection) throws RserveException {
+    rSession = connection.detach();
   }
 
   RConnection getRConnection() {
@@ -48,6 +74,10 @@ public class ArmadilloSession {
       }
       return rSession.attach();
     } catch (RserveException err) {
+      if(rSession == null) {
+        logger.error("Error creating connection", err);
+      }
+      logger.error("Error attaching connection", err);
       throw new ArmadilloSessionException("Could not attach connection to RSession", err);
     }
   }
@@ -61,6 +91,7 @@ public class ArmadilloSession {
         connection.close();
       }
     } catch (RserveException err) {
+      logger.error("Closing session and/or connection failed", err);
       throw new ArmadilloSessionException("Closing session and/or connection failed", err);
     }
   }
