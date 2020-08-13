feaa94387c8c9ad50dedd3dd9cc878594e9637a6
sidohaakma <sido@haakma.org>
2020-08-13 16:30:35 +0200

    chore: added more debugging for raw output

--- armadillo/src/main/java/org/molgenis/armadillo/ArmadilloUtils.java
+++ armadillo/src/main/java/org/molgenis/armadillo/ArmadilloUtils.java
@@ -1,13 +1,19 @@
 package org.molgenis.armadillo;
 
-import static java.lang.String.format;
-import static org.springframework.web.servlet.support.ServletUriComponentsBuilder.fromCurrentContextPath;
-
-import java.net.URI;
 import org.rosuda.REngine.REXP;
 import org.rosuda.REngine.REXPMismatchException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.net.URI;
+
+import static java.lang.String.format;
+import static java.nio.charset.StandardCharsets.UTF_8;
+import static org.springframework.web.servlet.support.ServletUriComponentsBuilder.fromCurrentContextPath;
 
 public class ArmadilloUtils {
+
+  private static final Logger LOGGER = LoggerFactory.getLogger(ArmadilloUtils.class);
 
   public static final String TABLE_ENV = ".DSTableEnv";
   public static final String GLOBAL_ENV = ".GlobalEnv";
@@ -22,7 +28,14 @@ public class ArmadilloUtils {
     byte[] rawResult = new byte[0];
     if (result.isRaw()) {
       try {
+        LOGGER.trace(format("RAW result : [ %s ]", new String(result.asBytes(), UTF_8)));
         rawResult = result.asBytes();
+      } catch (REXPMismatchException e) {
+        throw new IllegalStateException(e);
+      }
+    } else {
+      try {
+        LOGGER.debug(format("This was no 'RAW' result: [ %s ]", result.asNativeJavaObject()));
       } catch (REXPMismatchException e) {
         throw new IllegalStateException(e);
       }
