230b9150b30d7a257825792d459a284f1e77cc62
Fleur Kelpin <f.kelpin@umcg.nl>
2020-09-03 09:41:30 +0200

    fix: remove log spam

--- r/src/main/java/org/molgenis/r/RConnectionFactoryImpl.java
+++ r/src/main/java/org/molgenis/r/RConnectionFactoryImpl.java
@@ -1,8 +1,6 @@
 package org.molgenis.r;
 
 import org.molgenis.r.exceptions.ConnectionCreationFailedException;
-import org.rosuda.REngine.REXP;
-import org.rosuda.REngine.REXPMismatchException;
 import org.rosuda.REngine.Rserve.RConnection;
 import org.rosuda.REngine.Rserve.RserveException;
 import org.slf4j.Logger;
@@ -22,16 +20,14 @@ public class RConnectionFactoryImpl implements RConnectionFactory {
 
   @Override
   public RConnection retryCreateConnection() {
-    logger.info("retryCreateConnection");
+    logger.debug("retryCreateConnection");
     return createConnection();
   }
 
   @Override
   public RConnection createConnection() {
     try {
-      RConnection con = newConnection(rConfigProperties.getHost(), rConfigProperties.getPort());
-      logSessionInfo(con);
-      return con;
+      return newConnection(rConfigProperties.getHost(), rConfigProperties.getPort());
     } catch (RserveException ex) {
       throw new ConnectionCreationFailedException(ex);
     }
@@ -39,16 +35,5 @@ public class RConnectionFactoryImpl implements RConnectionFactory {
 
   RConnection newConnection(String host, int port) throws RserveException {
     return new RConnection(host, port);
-  }
-
-  private void logSessionInfo(RConnection con) throws RserveException {
-    if (logger.isDebugEnabled()) {
-      REXP rSessionInfo = con.eval("capture.output(sessionInfo())");
-      try {
-        logger.debug("Session info:\n{}", String.join("\n", rSessionInfo.asStrings()));
-      } catch (REXPMismatchException e) {
-        logger.warn("Error parsing session info.", e);
-      }
-    }
   }
 }
