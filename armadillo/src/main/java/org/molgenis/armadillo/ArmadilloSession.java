4854d582f43d3bd1f3a6d07460a4e88ceda4cf23
Fleur Kelpin <f.kelpin@umcg.nl>
2020-09-04 22:33:47 +0200

    fix: keep connection alive
    Do not detach, it is slow and buggy.
    
    Fixes #101

--- armadillo/src/main/java/org/molgenis/armadillo/ArmadilloSession.java
+++ armadillo/src/main/java/org/molgenis/armadillo/ArmadilloSession.java
@@ -2,103 +2,54 @@ package org.molgenis.armadillo;
 
 import static java.util.Objects.requireNonNull;
 
-import java.util.Optional;
 import java.util.function.Function;
-import org.molgenis.armadillo.exceptions.ArmadilloSessionException;
 import org.molgenis.armadillo.service.ArmadilloConnectionFactory;
 import org.molgenis.r.service.ProcessService;
 import org.rosuda.REngine.Rserve.RConnection;
-import org.rosuda.REngine.Rserve.RSession;
-import org.rosuda.REngine.Rserve.RserveException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class ArmadilloSession {
-  private RSession rSession = null;
-
   private static final Logger logger = LoggerFactory.getLogger(ArmadilloSession.class);
 
   private final ArmadilloConnectionFactory connectionFactory;
   private final ProcessService processService;
-  Integer pid;
+  private final RConnection connection;
+  final int pid;
+  private boolean busy = false;
 
   public ArmadilloSession(
       ArmadilloConnectionFactory connectionFactory, ProcessService processService) {
     this.connectionFactory = requireNonNull(connectionFactory);
-    this.processService = processService;
+    this.processService = requireNonNull(processService);
+    logger.trace("Creating new connection...");
+    connection = this.connectionFactory.createConnection();
+    pid = this.processService.getPid(connection);
+    logger.debug("Created new connection with pid {}.", pid);
   }
 
   public synchronized <T> T execute(Function<RConnection, T> consumer) {
-    RConnection connection = getRConnection();
+    busy = true;
     try {
       return consumer.apply(connection);
     } finally {
-      tryDetachRConnection(connection);
-    }
-  }
-
-  @SuppressWarnings("java:S3011")
-  private Optional<Integer> getPort() {
-    if (rSession == null) {
-      return Optional.empty();
-    }
-    try {
-      var portField = RSession.class.getDeclaredField("port");
-      portField.setAccessible(true);
-      return Optional.of(portField.getInt(rSession));
-    } catch (NoSuchFieldException | IllegalAccessException e) {
-      return Optional.empty();
-    }
-  }
-
-  void tryDetachRConnection(RConnection connection) {
-    try {
-      rSession = connection.detach();
-      if (logger.isDebugEnabled()) {
-        logger.debug("Detached session, port = {}", getPort());
-      }
-    } catch (RserveException e) {
-      logger.error("Failed to detach connection", e);
-      rSession = null;
-      pid = null;
-    }
-  }
-
-  RConnection getRConnection() {
-    try {
-      if (rSession == null) {
-        logger.trace("No existing session, creating new connection...");
-        var connection = connectionFactory.createConnection();
-        pid = processService.getPid(connection);
-        logger.debug("Created new connection with pid {}.", pid);
-        return connection;
-      }
-      logger.trace("Attaching to existing session with pid {}...", pid);
-      var connection = rSession.attach();
-      rSession = null;
-      logger.debug("Attached to existing session with pid {}.", pid);
-      return connection;
-    } catch (RserveException err) {
-      throw new ArmadilloSessionException("Could not attach connection to RSession", err);
+      busy = false;
     }
   }
 
   public void sessionCleanup() {
+    if (busy) {
+      killProcess();
+    }
+    connection.close();
+  }
+
+  private void killProcess() {
+    var conn = connectionFactory.createConnection();
     try {
-      if (rSession != null) {
-        logger.debug("Cleanup session");
-        RConnection connection = rSession.attach();
-        connection.close();
-      } else if (pid != null) {
-        var connection = connectionFactory.createConnection();
-        try {
-          processService.terminateProcess(connection, pid);
-        } finally {
-          connection.close();
-        }
-      }
-    } catch (RserveException err) {
-      throw new ArmadilloSessionException("Closing session and/or connection failed", err);
+      processService.terminateProcess(conn, pid);
+    } finally {
+      conn.close();
     }
   }
 }
