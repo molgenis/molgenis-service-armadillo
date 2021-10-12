5573bd50ff3f359bdcf1cf2778843909157a3bdc
Sido Haakma <sido@haakma.org>
2021-10-12 12:10:06 +0200

    fix: metrics endpoint for current rprocesses per environment

--- armadillo/src/main/java/org/molgenis/armadillo/info/RMetrics.java
+++ armadillo/src/main/java/org/molgenis/armadillo/info/RMetrics.java
@@ -1,15 +1,29 @@
 package org.molgenis.armadillo.info;
 
+import io.micrometer.core.instrument.Gauge;
+import io.micrometer.core.instrument.binder.MeterBinder;
+import org.molgenis.r.config.EnvironmentConfigProps;
+import org.molgenis.r.config.RServeConfig;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RMetrics {
-  // TODO: make it worrrrk!
-  //  @Bean
-  //  MeterBinder rProcesses(RProcessEndpoint processes) {
-  //    return registry ->
-  //        Gauge.builder("rserve.processes.current", processes::countRServeProcesses)
-  //            .description("Current number of RServe processes on the R server")
-  //            .register(registry);
-  //  }
+
+  @Bean
+  MeterBinder rProcesses(RServeConfig rServeConfig, RProcessEndpoint processes) {
+
+    return registry ->
+        rServeConfig.getEnvironments().stream()
+            .map(EnvironmentConfigProps::getName)
+            .forEach(
+                environment -> Gauge.builder(
+                        "rserve.processes.current." + environment,
+                        () -> processes.countRServeProcesses(environment))
+                    .description(
+                        "Current number of RServe processes on the R environment [ "
+                            + environment
+                            + " ]")
+                    .register(registry));
+  }
 }
