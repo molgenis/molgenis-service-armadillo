da42b1d85d70e57113c2df0e76604fb15f82947e
Fleur Kelpin <f.kelpin@umcg.nl>
2021-10-24 16:37:23 +0200

    feat: check that environment exists

--- armadillo/src/main/java/org/molgenis/armadillo/config/DataShieldConfigProps.java
+++ armadillo/src/main/java/org/molgenis/armadillo/config/DataShieldConfigProps.java
@@ -3,7 +3,10 @@ package org.molgenis.armadillo.config;
 import static org.molgenis.armadillo.profile.ActiveProfileNameAccessor.DEFAULT;
 
 import java.util.List;
+import javax.validation.Valid;
 import javax.validation.constraints.NotEmpty;
+import org.molgenis.r.config.EnvironmentConfigProps;
+import org.molgenis.r.config.RServeConfig;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.Errors;
@@ -14,10 +17,14 @@ import org.springframework.validation.annotation.Validated;
 @Component
 @Validated
 public class DataShieldConfigProps implements Validator {
-  @NotEmpty
-  private final List<ProfileConfigProps> profiles;
+  @NotEmpty @Valid private List<ProfileConfigProps> profiles;
+  private final RServeConfig rServeConfig;
 
-  public DataShieldConfigProps(List<ProfileConfigProps> profiles) {
+  public DataShieldConfigProps(RServeConfig rServeConfig) {
+    this.rServeConfig = rServeConfig;
+  }
+
+  public void setProfiles(List<ProfileConfigProps> profiles) {
     this.profiles = profiles;
   }
 
@@ -27,20 +34,35 @@ public class DataShieldConfigProps implements Validator {
 
   @Override
   public boolean supports(Class<?> clazz) {
-    return DataShieldConfigProps.class.isAssignableFrom(clazz);
+    return DataShieldConfigProps.class.isAssignableFrom(clazz)
+        || ProfileConfigProps.class.isAssignableFrom(clazz);
   }
 
   @Override
   public void validate(Object target, Errors errors) {
-    DataShieldConfigProps props = (DataShieldConfigProps) target;
-    if (props.getProfiles().stream()
-        .map(ProfileConfigProps::getName)
-        .noneMatch(DEFAULT::equals)) {
-      errors.rejectValue(
-          "profiles",
-          "default.profile.missing",
-          new Object[] {},
-          "Must specify a profile with name " + DEFAULT);
+    if (target instanceof ProfileConfigProps) {
+      var environmentName = ((ProfileConfigProps) target).getEnvironment();
+
+      if (rServeConfig.getEnvironments().stream()
+          .map(EnvironmentConfigProps::getName)
+          .noneMatch(environmentName::equals)) {
+        errors.rejectValue(
+            "environment",
+            "profile.environment.unknown",
+            new Object[] {},
+            "No RServe environment defined with name '" + environmentName + "'");
+      }
+    } else {
+      DataShieldConfigProps props = (DataShieldConfigProps) target;
+      if (props.getProfiles().stream()
+          .map(ProfileConfigProps::getName)
+          .noneMatch(DEFAULT::equals)) {
+        errors.rejectValue(
+            "profiles",
+            "datashield.profiles.missing-default",
+            new Object[] {},
+            "Must specify a profile with name " + DEFAULT);
+      }
     }
   }
 }
