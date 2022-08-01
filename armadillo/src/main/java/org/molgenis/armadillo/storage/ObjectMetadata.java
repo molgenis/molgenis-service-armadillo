8debc2ea21d0768222cd1944368e835dea06eac4
tommy <tommydeboer4@gmail.com>
2022-08-01 09:32:40 +0200

    add javadoc to ObjectMetadata

--- armadillo/src/main/java/org/molgenis/armadillo/storage/ObjectMetadata.java
+++ armadillo/src/main/java/org/molgenis/armadillo/storage/ObjectMetadata.java
@@ -4,6 +4,11 @@ import io.minio.messages.Item;
 import java.io.File;
 import java.util.Date;
 
+/**
+ * @param name The fully qualified name of the object (e.g. lifecycle/core/nonrep)
+ * @param lastModified The date & time the object was last modified
+ * @param size The size of the object in bytes
+ */
 public record ObjectMetadata(String name, Date lastModified, long size) {
   public static ObjectMetadata of(File file) {
     return new ObjectMetadata(file.getName(), new Date(file.lastModified()), file.length());
