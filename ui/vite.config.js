d69e09942eafa5b8894a556812906bf0dd9cbb9d
mswertz <m.a.swertz@rug.nl>
2022-08-09 08:42:12 +0200

    merge ui

--- ui/vite.config.js
+++ ui/vite.config.js
@@ -4,10 +4,5 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [vue()],
-    //for multi-module maven to bundle
-    build: {
-        emptyOutDir: true,
-        outDir: './target/classes/public'
-    }
 })
 
