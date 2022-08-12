6792ee4445c31d55b40771208a872440dd27417e
tommy <tommydeboer4@gmail.com>
2022-08-12 15:49:39 +0200

    move path matching configuration to WebConfig

--- armadillo/src/main/java/org/molgenis/armadillo/ArmadilloServiceApplication.java
+++ armadillo/src/main/java/org/molgenis/armadillo/ArmadilloServiceApplication.java
@@ -3,28 +3,15 @@ package org.molgenis.armadillo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.retry.annotation.EnableRetry;
-import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.util.UrlPathHelper;
 
 @SpringBootApplication(scanBasePackages = "org.molgenis")
 @EnableRetry
-public class ArmadilloServiceApplication implements WebMvcConfigurer {
+public class ArmadilloServiceApplication {
 
   public static void main(String[] args) {
     // Allow URL encoded slashes in Tomcat
     System.setProperty("org.apache.tomcat.util.buf.UDecoder.ALLOW_ENCODED_SLASH", "true");
 
     SpringApplication.run(ArmadilloServiceApplication.class, args);
-  }
-
-  /**
-   * Enable matching paths with URL encoded slashes. Needed for the Storage API's object endpoints.
-   */
-  @Override
-  public void configurePathMatch(PathMatchConfigurer configurer) {
-    UrlPathHelper urlPathHelper = new UrlPathHelper();
-    urlPathHelper.setUrlDecode(false);
-    configurer.setUrlPathHelper(urlPathHelper);
   }
 }
