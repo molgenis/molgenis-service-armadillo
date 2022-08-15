4396b02e86ec0392c58d5cb9fa0d0afd34ee486d
mkslofstra <mariska-karina@hotmail.com>
2022-08-15 16:48:38 +0200

    fix: fix proxy to api

--- ui/vite.config.js
+++ ui/vite.config.js
@@ -1,14 +1,19 @@
-import {defineConfig} from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [vue()],
-    server: {
-        port: "8080",
-        proxy: {
-          '/metadata/*': 'http://localhost:8080/metadata'
-          }
-      }
-})
-
+  plugins: [vue()],
+  server: {
+    port: "8081",
+    proxy: {
+      "^/metadata/.*": {
+        target: {
+          protocol: 'http:',
+          host: "localhost",
+          port: 8080,
+        },
+      },
+    },
+  },
+});
