b464bec4d588a96207531507ae025c8eec7d2ac3
mkslofstra <mariska-karina@hotmail.com>
2022-10-10 08:30:49 +0200

    test: add description to tests

--- ui/tests/unit/components/BadgeList.spec.ts
+++ ui/tests/unit/components/BadgeList.spec.ts
@@ -1,17 +1,19 @@
 import { mount } from "@vue/test-utils";
 import BadgeList from "@/components/BadgeList.vue";
 
-test("shows badges", () => {
-  const wrapper = mount(BadgeList, {
-    props: {
-      itemArray: ["item_a", "item_b", "item_c"],
-      saveCallback: jest.fn(),
-      row: { firstName: "James", lastName: "Doe" },
-    },
-  });
+describe("BadgeList", () => {
+  test("shows badges", () => {
+    const wrapper = mount(BadgeList, {
+      props: {
+        itemArray: ["item_a", "item_b", "item_c"],
+        saveCallback: jest.fn(),
+        row: { firstName: "James", lastName: "Doe" },
+      },
+    });
 
-  // Assert the rendered text of the componentgg
-  expect(wrapper.text()).toContain("item_a");
-  expect(wrapper.text()).toContain("item_b");
-  expect(wrapper.text()).toContain("item_c");
+    // Assert the rendered text of the componentgg
+    expect(wrapper.text()).toContain("item_a");
+    expect(wrapper.text()).toContain("item_b");
+    expect(wrapper.text()).toContain("item_c");
+  });
 });
--- ui/tests/unit/components/ButtonGroup.spec.ts
+++ ui/tests/unit/components/ButtonGroup.spec.ts
@@ -11,30 +11,32 @@ const wrapper = mount(ButtonGroup, {
   },
 });
 
-test("triggers function on click of button", () => {
-  // Assert the rendered text of the component
-  const buttons = wrapper.findAll("button");
-  buttons[0].trigger("click");
-  expect(mockFunction).toHaveBeenCalledWith("hello");
-});
+describe("ButtonGroup", () => {
+  test("triggers function on click of button", () => {
+    // Assert the rendered text of the component
+    const buttons = wrapper.findAll("button");
+    buttons[0].trigger("click");
+    expect(mockFunction).toHaveBeenCalledWith("hello");
+  });
 
-test("creates buttons with correct text and background colors", () => {
-  // Assert the rendered text of the component
-  const buttons = wrapper.findAll("button");
-  expect(buttons.length).toBe(3);
-  expect(buttons[0].attributes("class")).toContain("bg-danger");
-  expect(buttons[0].attributes("class")).toContain("btn-danger");
-  expect(buttons[1].attributes("class")).toContain("bg-info");
-  expect(buttons[1].attributes("class")).toContain("btn-primary");
-  expect(buttons[2].attributes("class")).toContain("bg-light");
-  expect(buttons[2].attributes("class")).toContain("btn-light");
-});
+  test("creates buttons with correct text and background colors", () => {
+    // Assert the rendered text of the component
+    const buttons = wrapper.findAll("button");
+    expect(buttons.length).toBe(3);
+    expect(buttons[0].attributes("class")).toContain("bg-danger");
+    expect(buttons[0].attributes("class")).toContain("btn-danger");
+    expect(buttons[1].attributes("class")).toContain("bg-info");
+    expect(buttons[1].attributes("class")).toContain("btn-primary");
+    expect(buttons[2].attributes("class")).toContain("bg-light");
+    expect(buttons[2].attributes("class")).toContain("btn-light");
+  });
 
-test("creates buttons with correct icons", () => {
-  // Assert the rendered text of the component
-  const icons = wrapper.findAll("i");
-  expect(icons.length).toBe(3);
-  expect(icons[0].attributes("class")).toContain("bi-pencil-fill");
-  expect(icons[1].attributes("class")).toContain("bi-trash-fill");
-  expect(icons[2].attributes("class")).toContain("bi-1-circle");
+  test("creates buttons with correct icons", () => {
+    // Assert the rendered text of the component
+    const icons = wrapper.findAll("i");
+    expect(icons.length).toBe(3);
+    expect(icons[0].attributes("class")).toContain("bi-pencil-fill");
+    expect(icons[1].attributes("class")).toContain("bi-trash-fill");
+    expect(icons[2].attributes("class")).toContain("bi-1-circle");
+  });
 });
