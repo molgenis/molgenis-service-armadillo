b6a053afbdbf3fbd4c8c72ae27e44a53b9d9631d
mkslofstra <mariska-karina@hotmail.com>
2022-10-10 10:16:24 +0200

    style: reformat

--- ui/tests/unit/components/FeedbackMessage.spec.ts
+++ ui/tests/unit/components/FeedbackMessage.spec.ts
@@ -1,17 +1,15 @@
 import { mount } from "@vue/test-utils";
 import FeedbackMessage from "@/components/FeedbackMessage.vue";
 
-describe("InlineRowEdit", () => {
-  test("displays message", () => {
-    const wrapper = mount(FeedbackMessage, {
-      props: {
-        successMessage: "Hello world",
-        errorMessage: "Destructing world in 3...2...1",
-      },
-    });
-
-    // Assert the rendered text of the component
-    expect(wrapper.text()).toContain("Hello world");
-    expect(wrapper.text()).toContain("Destructing world in 3...2...1");
+test("displays message", () => {
+  const wrapper = mount(FeedbackMessage, {
+    props: {
+      successMessage: "Hello world",
+      errorMessage: "Destructing world in 3...2...1",
+    },
   });
+
+  // Assert the rendered text of the component
+  expect(wrapper.text()).toContain("Hello world");
+  expect(wrapper.text()).toContain("Destructing world in 3...2...1");
 });
