65cc55d1146735fffe4ff93f8ff43adad774606e
mkslofstra <mariska-karina@hotmail.com>
2022-10-11 11:59:28 +0200

    test: add tests for Tabs

---
+++ ui/tests/unit/components/Tabs.spec.ts
@@ -0,0 +1,26 @@
+import { mount } from "@vue/test-utils";
+import Tabs from "@/components/Tabs.vue";
+
+describe("Tabs", () => {
+  const wrapper = mount(Tabs, {
+    props: {
+      menu: ["item_a", "item_b", "item_c"],
+      icons: ["monkey", "horse", "zebra"],
+      activeTab: 1
+    },
+  });
+  test("shows tabs with icons", () => {
+    // Assert the rendered text of the component
+    expect(wrapper.html()).toContain("item_a");
+    expect(wrapper.html()).toContain("item_b");
+    expect(wrapper.html()).toContain("item_c");
+    expect(wrapper.html()).toContain("bi-monkey");
+    expect(wrapper.html()).toContain("bi-horse");
+    expect(wrapper.html()).toContain("bi-zebra");
+  });
+
+  test("expect item_b to be active", () => {
+    const activeTab = wrapper.find('button.nav-link.active');
+    expect(activeTab.html()).toContain("item_b");
+  });
+});
