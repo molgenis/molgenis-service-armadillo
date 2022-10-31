90730dedd7461ee3947c2ee914b743c1afdc6810
mkslofstra <mariska-karina@hotmail.com>
2022-10-31 16:12:15 +0100

    fix(tabs): active tab not correct when navigating through URL
    Determine active tab based on URL instead of change on click of tab (which worked perfectly before
    using router-view)

--- ui/src/components/Tabs.vue
+++ ui/src/components/Tabs.vue
@@ -4,15 +4,14 @@
       <li v-for="(item, index) in menu" class="nav-item">
         <router-link :to="`/${item.toLowerCase()}`">
           <button
-              :class="index == activeTab ? 'nav-link active' : 'nav-link'"
-              :id="`${item}-tab`"
-              data-bs-toggle="tab"
-              :data-bs-target="`#${item}-tab-pane`"
-              type="button"
-              role="tab"
-              :aria-controls="`${item}-tab-pane`"
-              :aria-selected="index == activeTab ? 'true' : 'false'"
-              v-on:click="$emit('activeTabChange', index)"
+            :class="isSelectedPage(item) ? 'nav-link active' : 'nav-link'"
+            :id="`${item.toLowerCase()}-tab`"
+            data-bs-toggle="tab"
+            :data-bs-target="`#${item.toLowerCase()}-tab-pane`"
+            type="button"
+            role="tab"
+            :aria-controls="`${item.toLowerCase()}-tab-pane`"
+            :aria-selected="isSelectedPage(item) ? 'true' : 'false'"
           >
             <i v-if="index < icons.length" :class="`bi bi-${icons[index]}`"></i>
             {{ item }}
@@ -21,21 +20,32 @@
       </li>
     </ul>
     <div class="tab-content">
-      <router-view/>
+      <router-view />
     </div>
   </div>
 </template>
 
 <script lang="ts">
-import {StringArray} from "@/types/types";
-import {PropType} from "vue";
+import { StringArray } from "@/types/types";
+import { defineComponent, PropType } from "vue";
+import { RouterLink, RouterView } from "vue-router";
 
-export default {
+export default defineComponent({
   name: "Tabs",
   props: {
-    menu: {type: Array as PropType<StringArray>, required: true},
-    icons: {type: Array as PropType<StringArray>, required: true},
-    activeTab: {type: Number, required: true},
+    menu: { type: Array as PropType<StringArray>, required: true },
+    icons: { type: Array as PropType<StringArray>, required: true },
+    activeTab: { type: Number, required: true },
   },
-};
+  methods: {
+    isSelectedPage(page: string) {
+      return page.toLowerCase() === this.selectedPage;
+    },
+  },
+  computed: {
+    selectedPage() {
+      return this.$route.fullPath.split('/')[1];
+    },
+  },
+});
 </script>
