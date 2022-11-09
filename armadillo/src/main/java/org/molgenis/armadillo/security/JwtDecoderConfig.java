bbc4b8ff23dde0baf6798f24d1a3257024bf532f
mswertz <m.a.swertz@rug.nl>
2022-11-09 22:09:46 +0100

    chore: allow auth server online in development, not prod profile

--- armadillo/src/main/java/org/molgenis/armadillo/security/JwtDecoderConfig.java
+++ armadillo/src/main/java/org/molgenis/armadillo/security/JwtDecoderConfig.java
@@ -3,18 +3,14 @@ package org.molgenis.armadillo.security;
 import static org.springframework.security.oauth2.jwt.JwtClaimNames.AUD;
 
 import java.util.Collection;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.autoconfigure.security.oauth2.resource.OAuth2ResourceServerProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.oauth2.core.DelegatingOAuth2TokenValidator;
 import org.springframework.security.oauth2.core.OAuth2TokenValidator;
-import org.springframework.security.oauth2.jwt.Jwt;
-import org.springframework.security.oauth2.jwt.JwtClaimValidator;
-import org.springframework.security.oauth2.jwt.JwtDecoder;
-import org.springframework.security.oauth2.jwt.JwtDecoders;
-import org.springframework.security.oauth2.jwt.JwtValidators;
-import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.jwt.*;
 
 @ConditionalOnProperty(
     prefix = "spring.security.oauth2.resourceserver",
@@ -22,19 +18,39 @@ import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 @Configuration
 public class JwtDecoderConfig {
 
+  @Value("${spring.profiles.active}")
+  private String activeProfile;
+
   @Bean
   public JwtDecoder jwtDecoder(OAuth2ResourceServerProperties properties) {
-    String issuerUri = properties.getJwt().getIssuerUri();
-    NimbusJwtDecoder jwtDecoder = (NimbusJwtDecoder) JwtDecoders.fromIssuerLocation(issuerUri);
+    try {
+      String issuerUri = properties.getJwt().getIssuerUri();
+      NimbusJwtDecoder jwtDecoder = (NimbusJwtDecoder) JwtDecoders.fromIssuerLocation(issuerUri);
 
-    var audienceValidator =
-        new JwtClaimValidator<Collection<String>>(
-            AUD, aud -> aud != null && aud.contains(properties.getOpaquetoken().getClientId()));
-    OAuth2TokenValidator<Jwt> jwtValidator =
-        new DelegatingOAuth2TokenValidator<>(
-            JwtValidators.createDefaultWithIssuer(issuerUri), audienceValidator);
+      var audienceValidator =
+          new JwtClaimValidator<Collection<String>>(
+              AUD, aud -> aud != null && aud.contains(properties.getOpaquetoken().getClientId()));
+      OAuth2TokenValidator<Jwt> jwtValidator =
+          new DelegatingOAuth2TokenValidator<>(
+              JwtValidators.createDefaultWithIssuer(issuerUri), audienceValidator);
 
-    jwtDecoder.setJwtValidator(jwtValidator);
-    return jwtDecoder;
+      jwtDecoder.setJwtValidator(jwtValidator);
+      return jwtDecoder;
+    } catch (Exception e) {
+      // in development we will not fail but allow offline development
+      if ("development".equals(activeProfile)) {
+        // how to elegantly fail if the provided issuer is not responding?
+        e.printStackTrace();
+        return new JwtDecoder() {
+          @Override
+          public Jwt decode(String token) throws JwtException {
+            throw new UnsupportedOperationException(
+                "JWT configuration failed, please check the logs. Probably the auth server is offline?");
+          }
+        };
+      } else {
+        throw e;
+      }
+    }
   }
 }
