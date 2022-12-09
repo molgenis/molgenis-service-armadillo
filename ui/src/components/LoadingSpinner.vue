11aa8b09b3828114372150e486e351e1b73de982
mkslofstra <mariska-karina@hotmail.com>
2022-12-09 08:49:20 +0100

    style(loadingspinner): replace boring spinner with cool armadillo spinner

--- ui/src/components/LoadingSpinner.vue
+++ ui/src/components/LoadingSpinner.vue
@@ -1,13 +1,65 @@
 <template>
-  <div class="d-flex justify-content-center">
-    <div class="spinner-border" role="status">
-      <span class="visually-hidden">Loading...</span>
-    </div>
+  <div class="loading-spinner p-0 m-0">
+    <img
+      class="molgenis p-0 mx-auto d-block"
+      :style="styleObject"
+      src="/molgenis-logo.png"
+    />
+    <img
+      class="p-0 mx-auto d-block spinner-logo"
+      :width="this.imageWidth"
+      src="/armadillo-logo-loading.png"
+    />
   </div>
 </template>
 
 <script lang="ts">
 export default {
   name: "LoadingSpinner",
+  props: {
+    imageWidth: {
+      type: String,
+      default: "70",
+    },
+  },
+  computed: {
+    width(){
+      return Number(this.imageWidth);
+    },
+    styleObject() {
+      return {
+        "width": `${this.width - this.width * 0.2}px`,
+        "margin-bottom": `-${this.width}px`,
+      };
+    },
+  },
 };
 </script>
+
+<style lang="css" scoped>
+.molgenis {
+  opacity: 0.7;
+}
+.spinner-logo {
+  opacity: 0.7;
+  transform-origin: 49% 58%;
+  animation: rotation 2s infinite linear;
+}
+.rotate {
+  animation: rotation 2s;
+}
+@keyframes rotation {
+  from {
+    transform: rotate(359deg);
+  }
+  to {
+    transform: rotate(0deg);
+  }
+}
+.linear {
+  animation-timing-function: linear;
+}
+.infinite {
+  animation-iteration-count: infinite;
+}
+</style>
