f1b8a8228d95bef418086f830ed6d0697df66e7e
tommy <tommydeboer4@gmail.com>
2022-12-14 14:08:30 +0100

    feat: add custom spring security expression "canLoadToProfile"

---
+++ armadillo/src/main/java/org/molgenis/armadillo/security/ProfileSecurity.java
@@ -0,0 +1,29 @@
+package org.molgenis.armadillo.security;
+
+import static org.molgenis.armadillo.security.RunAs.runAsSystem;
+
+import org.molgenis.armadillo.exceptions.ProfileNotAllowedException;
+import org.molgenis.armadillo.metadata.AccessService;
+import org.molgenis.armadillo.profile.ActiveProfileNameAccessor;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ProfileSecurity {
+  private final AccessService accessService;
+
+  public ProfileSecurity(AccessService accessService) {
+    this.accessService = accessService;
+  }
+
+  @PreAuthorize("hasAnyRole('ROLE_SU', 'ROLE_' + #project.toUpperCase() + '_RESEARCHER')")
+  public boolean canLoadToProfile(String project) {
+    var profile = ActiveProfileNameAccessor.getActiveProfileName();
+    var allowedProfiles = runAsSystem(() -> accessService.projectsByName(project).getProfiles());
+    if (!allowedProfiles.contains(profile)) {
+      // throw exception instead of returning false, so we get a 403 with a friendly message
+      throw new ProfileNotAllowedException(project, profile, allowedProfiles);
+    }
+    return true;
+  }
+}
