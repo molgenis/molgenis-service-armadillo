6420ccca403104c20ea855114bfe328baf01d59b
DickPostma <f.d.postma@umcg.nl>
2023-02-22 15:03:09 +0100

    added Documentation

--- scripts/install/armadillo-check-update.sh
+++ scripts/install/armadillo-check-update.sh
@@ -1,36 +1,19 @@
 #!/bin/bash
 
 ARMADILLO_UPDATESCR_VERSION=0.0.1
+ARMADILLO_URL=https://github.com/molgenis/molgenis-service-armadillo/
+ARMADILLO_PATH=/usr/share/armadillo
 
-check_self_update() {
-  UPDATE_SCRIPT=$(curl -L -s https://raw.githubusercontent.com/DickPostma/molgenis-service-armadillo/installScript/scripts/install/armadillo-check-update.sh)
-  UPDATE_VERSION=$(echo "$UPDATE_SCRIPT" | sed -e 's/.*"ARMADILLO_UPDATESCR_VERSION"="\([^"]*\)".*/\1/')
 
-  echo $UPDATE_VERSION
-  exit
-    
-  if validate_url $DL_URL; then
-    
-    wget -q -O $ARMADILLO_PATH/application/armadillo-update.sh "$DL_URL"
-    echo "Update script downloaded"
-    echo "1 0 * * 0 bash $ARMADILLO_PATH/application/armadillo-update.sh" >> /etc/cron.d/update-armadillo
-  
-  else
-    echo "[ ERROR ] update script not downloaded"
-  fi
 
-   
-}
 
 check_armadillo_update() {
 
-  FILE=$(readlink "$ARMADILLO_PATH/application/armadillo.jar")
   VERSION_USED=$(curl -L -s -H 'Accept: application/json' -s http://localhost:8080/actuator/info | sed -e 's/.*"version":"\([^"]*\)".*/\1/')
-    
   LATEST_RELEASE=$(curl -L -s -H 'Accept: application/json' -s $ARMADILLO_URL/releases/latest)
   ARMADILLO_LATEST_VERSION=$(echo "$LATEST_RELEASE" | sed -e 's/.*"tag_name":"\([^"]*\)".*/\1/' | sed -e s/.*armadillo-service-//)
 
-    if [[ "$ARMADILLO_LATEST_VERSION" =~ "2.2.3" ]]; then
+    if [[ "$ARMADILLO_LATEST_VERSION" =~ 2.2.3 ]]; then
   {
     
     echo "Update of Armadillo 2 version is not supported on this system"
@@ -74,5 +57,5 @@ function validate_url(){
 }
 
 
-check_self_update
+
 check_armadillo_update
