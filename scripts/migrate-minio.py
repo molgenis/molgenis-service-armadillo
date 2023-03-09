163d143467e0b97bd01e0ff8d33f86b4d90b3350
mkslofstra <mariska-karina@hotmail.com>
2023-03-09 08:47:47 +0100

    fix: convert posixpath to string

--- scripts/migrate-minio.py
+++ scripts/migrate-minio.py
@@ -103,8 +103,8 @@ def download_objects(bucket_name: str, bucket_dir: Path, client: Minio) -> int:
     for obj in objects:
         print("- " + obj.object_name)
         target_file = bucket_dir.joinpath(obj.object_name)
-        if target_file.endswith(".rds"):
-            skipped.append(target_file)
+        if str(target_file.endswith(".rds")):
+            skipped.append(str(target_file))
         else:
             client.fget_object(
                 bucket_name,
