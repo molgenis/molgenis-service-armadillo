4f3d405265373499441bad12996fbb6e1d762a56
mswertz <m.a.swertz@rug.nl>
2023-07-30 12:30:51 +0200

    update from gliderpilot to nyx release plugin

--- armadillo/src/test/java/org/molgenis/armadillo/TestSecurityConfig.java
+++ armadillo/src/test/java/org/molgenis/armadillo/TestSecurityConfig.java
@@ -14,6 +14,7 @@ import org.molgenis.armadillo.profile.ProfileScope;
 import org.molgenis.armadillo.storage.ArmadilloStorageService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.GrantedAuthority;
@@ -31,10 +32,8 @@ public class TestSecurityConfig {
   @Bean
   protected SecurityFilterChain configure(HttpSecurity http) throws Exception {
     return http.authorizeHttpRequests(request -> request.anyRequest().authenticated())
-        .httpBasic()
-        .and()
-        .csrf()
-        .disable()
+        .httpBasic(Customizer.withDefaults())
+        .csrf(csrf -> csrf.disable())
         .build();
   }
 
