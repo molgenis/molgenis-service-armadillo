bd8dfddc6add9869da551b715ca113f4705dc450
SietsmaRJ <work.robertsietsma@gmail.com>
2023-09-14 09:43:58 +0200

    Finalized script. Untested.

--- scripts/import-users.py
+++ scripts/import-users.py
@@ -12,7 +12,9 @@ import pandas as pd
 def main():
     server, data_directory = CommandLineParser().parse()
     api = API(server)
+    cohorts = ProjectDataParser().parse(data_directory)
     processor = ProjectProcessor(api)
+    processor.parse(cohorts)
 
 
 class ProjectDataParser:
@@ -25,8 +27,10 @@ class ProjectDataParser:
             full_path = os.path.join(directory, file)
             project_name = self._parse_filename(file)
             if project_name == "uncategorized users":
-                pass
-
+                return_dict["NaN"] = self._load_tsv(full_path)
+            else:
+                return_dict[project_name] = self._load_tsv(full_path)
+        return return_dict
 
     @staticmethod
     def _parse_filename(filename):
@@ -35,19 +39,76 @@ class ProjectDataParser:
         filename = filename.replace()
         return filename
 
+    @staticmethod
+    def _load_tsv(path):
+        try:
+            data = pd.read_csv(
+                path,
+                sep='\t',
+                dtype={
+                    "active": bool,
+                    "email": str,
+                    "firstName": str,
+                    "fullName": str,
+                    "lastName": str,
+                    "roles": str
+                }
+            ).fillna("")
+        except pd.errors.EmptyDataError:
+            warnings.warn(f"Obtaining {path} resulted in empty dataframe!")
+            data = pd.DataFrame(
+                columns=["active", "email", "firstName", "fullName", "lastName", "roles"]
+            )
+        return data
+
 
 class ProjectProcessor:
     def __init__(self, api):
         self.api = api
+        self.su = []
 
-    def get_users(self):
-        self.api.get_users()
+    def parse(self, cohort_data):
+        for cohort_name, cohort_users in cohort_data.items():
+            if cohort_name == "NaN":
+                continue
+            self._obtain_su(cohort_users)
+            self._put_project(name=cohort_name, users=cohort_users['email'].values.tolist())
+        self._process_uncategorized_users(cohort_data['NaN'])
+        self._add_admin_rights()
 
-    def put_user(self, user_data):
-        self.api.put_user(user_data)
+    def _put_user(self, email, firstName, lastName):
+        self.api.put_user({
+              "email": email,
+              "firstName": firstName,
+              "lastName": lastName,
+              "admin": False,
+        })
 
-    def put_project(self, project_data):
-        self.api.put_project(project_data)
+    def _put_project(self, name, users):
+        self.api.put_project({"name": name, "users": users})
+
+    def _obtain_su(self, project_data):
+        su = project_data.loc[
+                project_data[project_data['roles'].isin(("SU", "ADMIN", "admin"))].index,
+                "email"
+            ]
+        for user in su:
+            if user not in self.su:
+                self.su.append(user)
+
+    def _process_uncategorized_users(self, uncategorized_users):
+        self._obtain_su(uncategorized_users)
+        uncategorized_users.apply(
+            lambda x: self._put_user(
+                email=x['email'],
+                firstName=x['firstName'],
+                lastName=x['lastName']
+            )
+        )
+
+    def _add_admin_rights(self):
+        for user in self.su:
+            self.api.put_user({"email": user, "admin": True})
 
 
 class API:
@@ -70,17 +131,9 @@ class API:
         response = requests.put(self.target + "access/users", json=data, headers=self.header)
         response.raise_for_status()
 
-    def get_users(self):
-        response = requests.get(self.target + "access/users", headers=self.header).json()
-        return response
-
     def put_project(self, project):
         response = requests.put(self.target + "access/projects", json=project, headers=self.header)
         response.raise_for_status()
-
-    def get_projects(self):
-        response = requests.get(self.target + "access/projects", headers=self.header).json()
-        return response
 
 
 class CommandLineParser:
