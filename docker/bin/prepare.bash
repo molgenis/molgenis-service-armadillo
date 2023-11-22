bbddbb2304800725d82ea7648b6595295c79412d
Clemens Tolboom <c.g.w.m.tolboom@umcg.nl>
2023-11-22 16:11:35 +0100

    Move script up for reuse on CI.

--- docker/bin/prepare.bash
+++ docker/bin/prepare.bash
@@ -1,20 +1,24 @@
 #!/usr/bin/env bash
 
+[[ "$1" == "" ]] && exit 1
+
 # Store the current working directory
 cwd=$(pwd)
 
 # Get the directory of the script
-SCRIPT_DIR="$(dirname "$0")"
+FULL_PATH="$(realpath "$0")"
+SCRIPT_DIR="$(dirname "$FULL_PATH")"
 DOCKER_DIR="$(dirname "$SCRIPT_DIR")"
+TARGET_DIR="$DOCKER_DIR/$1"
 
-cd "$DOCKER_DIR" || exit 1
+cd "$TARGET_DIR" || exit 1
 
-FAKE_DIR="$DOCKER_DIR/fake-tree/"
+FAKE_DIR="$TARGET_DIR/fake-tree/"
 
 PROJECT_DIR="$(git rev-parse --show-toplevel)"
 echo "Project dir   : $PROJECT_DIR"
-echo "Docker        : $DOCKER_DIR"
 echo "Script dir    : $SCRIPT_DIR"
+echo "Target        : $TARGET_DIR"
 echo "Fake tree dir : $FAKE_DIR"
 
 cd "$PROJECT_DIR"
@@ -28,4 +32,3 @@ mkdir -p "$FAKE_DIR/data/system"
 cp -r "$PROJECT_DIR/data/shared-lifecycle" "$FAKE_DIR/data/"
 
 cd "$cwd" || exit 1
-
