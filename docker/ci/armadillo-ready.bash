fada0004558c208d2415654843fc610852537fb2
Clemens Tolboom <c.g.w.m.tolboom@umcg.nl>
2023-11-27 12:06:34 +0100

    Attach fake-tree volume (for post mortom).

--- docker/ci/armadillo-ready.bash
+++ docker/ci/armadillo-ready.bash
@@ -8,8 +8,7 @@ for ((i=1; i<=duration; i++))
 do
   echo "$i: is Armadillo up?"
   if wget --spider "http://${HOSTNAME}:8080"; then
-  #if [ $? -eq 0 ]; then
-    echo "Armadillo is up"
+    echo "Armadillo is up according to wget ..."
     ./release-test.R || exit 1
     exit 0
   else
