971125f570d7b35de1a994b3279267c92adae139
Clemens Tolboom <c.g.w.m.tolboom@umcg.nl>
2024-01-08 09:36:58 +0100

    Cleanup fileService usage.

--- armadillo/src/main/java/org/molgenis/armadillo/metadata/InsightService.java
+++ armadillo/src/main/java/org/molgenis/armadillo/metadata/InsightService.java
@@ -12,6 +12,14 @@ import org.springframework.stereotype.Service;
 @Service
 @PreAuthorize("hasRole('ROLE_SU')")
 public class InsightService {
+
+  private final FileService fileService;
+
+  public InsightService(FileService fileService) {
+    this.fileService = fileService;
+  }
+  ;
+
   public List<FileInfo> filesInfo() {
     ArrayList<FileInfo> list = new ArrayList<>();
     list.add(FileInfo.create("LOG_FILE", "Log file"));
@@ -39,11 +47,11 @@ public class InsightService {
   public FileDetails fileDetails(String file_id) {
     return switch (file_id) {
       case "LOGGER_INFO" -> FileDetails.create(
-          file_id, "Logger info", (new FileService()).DumpLoggers(), getServerTime());
+          file_id, "Logger info", this.fileService.DumpLoggers(), getServerTime());
       case "LOG_FILE" -> FileDetails.create(
-          file_id, "Log file", (new FileService()).readLogFile(logFilePath), getServerTime());
+          file_id, "Log file", this.fileService.readLogFile(logFilePath), getServerTime());
       case "AUDIT_FILE" -> FileDetails.create(
-          file_id, "Audit file", (new FileService()).readLogFile(auditFilePath), getServerTime());
+          file_id, "Audit file", this.fileService.readLogFile(auditFilePath), getServerTime());
       case "CONFIG" -> FileDetails.create(file_id, "Configuration", "tbd", getServerTime());
       default -> FileDetails.create(file_id, file_id, file_id, getServerTime());
     };
@@ -52,11 +60,11 @@ public class InsightService {
   public FileDetails downloadFile(String file_id) {
     return switch (file_id) {
       case "LOGGER_INFO" -> FileDetails.create(
-          file_id, "Logger info", (new FileService()).DumpLoggers(), getServerTime());
+          file_id, "Logger info", this.fileService.DumpLoggers(), getServerTime());
       case "LOG_FILE" -> FileDetails.create(
-          file_id, "Log file", (new FileService()).readLogFile(logFilePath), getServerTime());
+          file_id, "Log file", this.fileService.readLogFile(logFilePath), getServerTime());
       case "AUDIT_FILE" -> FileDetails.create(
-          file_id, "Audit file", (new FileService()).readLogFile(auditFilePath), getServerTime());
+          file_id, "Audit file", this.fileService.readLogFile(auditFilePath), getServerTime());
       case "CONFIG" -> FileDetails.create(file_id, "Configuration", "tbd", getServerTime());
       default -> FileDetails.create(file_id, file_id, file_id, getServerTime());
     };
