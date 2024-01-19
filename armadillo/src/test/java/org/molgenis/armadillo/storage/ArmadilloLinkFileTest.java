03f4e56c88339221fd540da5d07303d5cfed5be4
mkslofstra <mariska-karina@hotmail.com>
2024-01-19 12:22:25 +0100

    test(ArmadilloLinkFile): add tests for initiating ArmadilloLinkFile from stream

--- armadillo/src/test/java/org/molgenis/armadillo/storage/ArmadilloLinkFileTest.java
+++ armadillo/src/test/java/org/molgenis/armadillo/storage/ArmadilloLinkFileTest.java
@@ -1,8 +1,10 @@
 package org.molgenis.armadillo.storage;
 
+import static java.lang.String.format;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import com.google.gson.JsonObject;
+import com.google.gson.JsonParseException;
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
 import org.junit.jupiter.api.Test;
@@ -11,8 +13,12 @@ public class ArmadilloLinkFileTest {
   String srcObj = "folder/src-obj";
   String srcProject = "src-project";
   String vars = "var1,var2,var3";
-  ArmadilloLinkFile alf =
-      new ArmadilloLinkFile(srcProject, srcObj, vars, "folder/link-obj", "view-project");
+  String testData =
+      "{\"sourceObject\":\"folder/src-obj\",\"sourceProject\":\"src-project\",\"variables\":\"var1,var2,var3\"}";
+  String linkObj = "folder/link-obj";
+
+  String linkProject = "view-project";
+  ArmadilloLinkFile alf = new ArmadilloLinkFile(srcProject, srcObj, vars, linkObj, linkProject);
 
   @Test
   public void testBuildJson() {
@@ -25,9 +31,7 @@ public class ArmadilloLinkFileTest {
   @Test
   public void testToString() {
     String actual = alf.toString();
-    String expected =
-        "{\"sourceObject\":\"folder/src-obj\",\"sourceProject\":\"src-project\",\"variables\":\"var1,var2,var3\"}";
-    assertEquals(expected, actual);
+    assertEquals(testData, actual);
   }
 
   @Test
@@ -38,9 +42,70 @@ public class ArmadilloLinkFileTest {
 
   @Test
   public void testLoadFromStream() {
-    String testString = "{\"test\":\"test\"}";
-    InputStream inputStream = new ByteArrayInputStream(testString.getBytes());
+    InputStream inputStream = new ByteArrayInputStream(testData.getBytes());
     JsonObject actual = alf.loadFromStream(inputStream);
-    assertEquals(testString, actual.toString());
+    assertEquals(testData, actual.toString());
+  }
+
+  @Test
+  public void testLoadLinkFileFromStream() {
+    InputStream inputStream = new ByteArrayInputStream(testData.getBytes());
+    ArmadilloLinkFile alfFromStream = new ArmadilloLinkFile(inputStream, linkObj, linkProject);
+    JsonObject jsonFromStream = alfFromStream.buildJson();
+    assertEquals(srcObj, jsonFromStream.get("sourceObject").getAsString());
+    assertEquals(srcProject, jsonFromStream.get("sourceProject").getAsString());
+    assertEquals(vars, jsonFromStream.get("variables").getAsString());
+  }
+
+  @Test
+  public void testLoadLinkFileFromStreamInvalidJson() {
+    String testData =
+        "\"sourceObject\":\"folder/src-obj\",\"sourceProject\":\"src-project\",\"variables\":\"var1,var2,var3\"}";
+    InputStream inputStream = new ByteArrayInputStream(testData.getBytes());
+    try {
+      new ArmadilloLinkFile(inputStream, linkObj, linkProject);
+    } catch (JsonParseException e) {
+      String message = format("Cannot load [%s/%s] because JSON is invalid", linkProject, linkObj);
+      assertEquals(message, e.getMessage());
+    }
+  }
+
+  @Test
+  public void testLoadLinkFileFromStreamInvalidObj() {
+    String testData =
+        "{\"sourceObj\":\"folder/src-obj\",\"sourceProject\":\"src-project\",\"variables\":\"var1,var2,var3\"}";
+    InputStream inputStream = new ByteArrayInputStream(testData.getBytes());
+    try {
+      new ArmadilloLinkFile(inputStream, linkObj, linkProject);
+    } catch (NullPointerException e) {
+      String message = format("Source object is missing from [%s/%s]", linkProject, linkObj);
+      assertEquals(message, e.getMessage());
+    }
+  }
+
+  @Test
+  public void testLoadLinkFileFromStreamInvalidProject() {
+    String testData =
+        "{\"sourceObject\":\"folder/src-obj\",\"sourceProj\":\"src-project\",\"variables\":\"var1,var2,var3\"}";
+    InputStream inputStream = new ByteArrayInputStream(testData.getBytes());
+    try {
+      new ArmadilloLinkFile(inputStream, linkObj, linkProject);
+    } catch (NullPointerException e) {
+      String message = format("Source project is missing from [%s/%s]", linkProject, linkObj);
+      assertEquals(message, e.getMessage());
+    }
+  }
+
+  @Test
+  public void testLoadLinkFileFromStreamInvalidVariables() {
+    String testData =
+        "{\"sourceObject\":\"folder/src-obj\",\"sourceProject\":\"src-project\",\"variabls\":\"var1,var2,var3\"}";
+    InputStream inputStream = new ByteArrayInputStream(testData.getBytes());
+    try {
+      new ArmadilloLinkFile(inputStream, linkObj, linkProject);
+    } catch (NullPointerException e) {
+      String message = format("Variables are not defined on [%s/%s]", linkProject, linkObj);
+      assertEquals(message, e.getMessage());
+    }
   }
 }
