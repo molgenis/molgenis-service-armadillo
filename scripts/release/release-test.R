c01f00a53761535d1b642a524e83118a643b3366
Tim Cadman <41470917+timcadman@users.noreply.github.com>
2024-02-21 15:13:58 +0100

    test: use two tokens to test 2 cohort setup

--- scripts/release/release-test.R
+++ scripts/release/release-test.R
@@ -967,8 +967,11 @@ verify_ne_model_class()
 verify_ne_imp_class()
 verify_ne_lht_class()
 
-logindata_1 <- create_dsi_builder(server = "testserver1", url = armadillo_url, profile = profile, password = admin_pwd, token = token, table = sprintf("%s/2_1-core-1_0/nonrep", project1))
-logindata_2 <- create_dsi_builder(server = "testserver2", url = armadillo_url, profile = profile, password = admin_pwd, token = token, table = sprintf("%s/2_1-core-1_0/nonrep", project1))
+token_1 <- armadillo.get_token(armadillo_url)
+token_2 <- armadillo.get_token(armadillo_url)
+
+logindata_1 <- create_dsi_builder(server = "testserver1", url = armadillo_url, profile = profile, password = admin_pwd, token = token_1, table = sprintf("%s/2_1-core-1_0/nonrep", project1))
+logindata_2 <- create_dsi_builder(server = "testserver2", url = armadillo_url, profile = profile, password = admin_pwd, token = token_2, table = sprintf("%s/2_1-core-1_0/nonrep", project1))
 logindata <- rbind(logindata_1, logindata_2) #This allows us to test two servers (required for dsMTL)
 
 conns <- DSI::datashield.login(logins = logindata, assign = T, symbol = "nonrep")
