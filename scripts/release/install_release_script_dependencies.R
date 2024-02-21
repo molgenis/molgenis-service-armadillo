4903c88f5857500b0ddf25af59fb89a26a17258f
Tim Cadman <41470917+timcadman@users.noreply.github.com>
2024-02-21 14:57:46 +0100

    test: added version for dsMediationClient

--- scripts/release/install_release_script_dependencies.R
+++ scripts/release/install_release_script_dependencies.R
@@ -55,7 +55,7 @@ warnings()
 
 library("devtools")
 install_github("datashield/dsBaseClient")
-install_github("datashield/dsMediationClient")
+install_github("datashield/dsMediationClient", ref = "0.0.3")
 install_github("https://github.com/transbioZI/dsMTLClient", ref = "0.9.9")
 
 #check if all packages are installed
