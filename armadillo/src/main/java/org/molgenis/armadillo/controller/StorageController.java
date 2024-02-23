e347bc1ca70c800eb42f42de8f9fc7b37bf1b558
mkslofstra <mariska-karina@hotmail.com>
2024-02-23 15:35:31 +0100

    refactor: roll back changes in requestmapping

--- armadillo/src/main/java/org/molgenis/armadillo/controller/StorageController.java
+++ armadillo/src/main/java/org/molgenis/armadillo/controller/StorageController.java
@@ -37,7 +37,16 @@ import org.molgenis.armadillo.storage.FileInfo;
 import org.springframework.core.io.InputStreamResource;
 import org.springframework.http.*;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.server.ResponseStatusException;
 
@@ -154,10 +163,9 @@ public class StorageController {
         @ApiResponse(responseCode = "409", description = "Object already exists"),
         @ApiResponse(responseCode = "401", description = "Unauthorized")
       })
-  @RequestMapping(
+  @PostMapping(
       value = "/projects/{project}/objects/link",
-      consumes = {APPLICATION_JSON_VALUE},
-      method = RequestMethod.POST)
+      consumes = {APPLICATION_JSON_VALUE})
   @ResponseStatus(NO_CONTENT)
   public void createLinkedObject(
       Principal principal,
