4f19c27f1fdc8d351c9f19e0101e0cd3aa6f7033
Tim Cadman <41470917+timcadman@users.noreply.github.com>
2024-02-27 10:41:27 +0100

    test: removed and added whitespace

--- scripts/release/xenon-survival.R
+++ scripts/release/xenon-survival.R
@@ -1,5 +1,4 @@
-create_survival_object <- function(){
-
+create_survival_object <- function() {
   dsSurvivalClient::ds.Surv(
     time='survival$time',
     event = 'survival$status',
@@ -7,73 +6,68 @@ create_survival_object <- function(){
 
 }
 
-verify_survival_class <- function(){
-
+verify_survival_class <- function() {
   surv_class <- ds.class("surv_object")
 
-  if(surv_class$armadillo == "Surv"){
+  if(surv_class$armadillo == "Surv") {
     cli_alert_success("ds.survival passed")
-  } else{
+  } else {
     cli_alert_danger("ds.survival failed")
     exit_test("ds.survival did not create a serverside object with the expected class")
   }
 
 }
 
-verify_cox_output <- function(){
-
+verify_cox_output <- function() {
   cox_output <- dsSurvivalClient::ds.coxph.SLMA(formula = 'surv_object~survival$age')
   expected_names <- c("call", "fail", "na.action", "n", "loglik", "nevent", "coefficients",
                       "conf.int", "logtest", "sctest", "rsq", "waldtest", "used.robust", "concordance")
 
-  if(identical(names(cox_output$armadillo), expected_names)){
+  if(identical(names(cox_output$armadillo), expected_names)) {
     cli_alert_success("ds.coxph.SLMA passed")
-  } else{
+  } else {
     cli_alert_danger("ds.coxph.SLMA failed")
     exit_test("ds.coxph.SLMA did not create a list with the expected elements")
   }
 
 }
 
-verify_cox_phSLMAassign_class <- function(){
-
+verify_cox_phSLMAassign_class <- function() {
   dsSurvivalClient::ds.coxphSLMAassign(formula = 'surv_object~survival$age',
                                        objectname = 'coxph_serverside')
 
   cox_class <- ds.class("coxph_serverside")
 
-  if(cox_class$armadillo == "coxph"){
+  if(cox_class$armadillo == "coxph") {
     cli_alert_success("ds.coxphSLMAassign passed")
-  } else{
+  } else {
     cli_alert_danger("ds.coxphSLMAassign failed")
     exit_test("ds.coxphSLMAassign did not create a serverside object with the expected class")
   }
 
 }
 
-verify_cox_zphSLMA_object <- function(){
-
+verify_cox_zphSLMA_object <- function() {
   hazard_assumption <- dsSurvivalClient::ds.cox.zphSLMA(fit = 'coxph_serverside')
   expected_names <- c("table", "var", "transform", "call")
 
-  if(identical(names(hazard_assumption$armadillo), expected_names)){
+  if(identical(names(hazard_assumption$armadillo), expected_names)) {
     cli_alert_success("ds.cox.zphSLMA passed")
-  } else{
+  } else {
     cli_alert_danger("ds.cox.zphSLMA failed")
     exit_test("ds.cox.zphSLMA did not create a list with the expected elements")
   }
 
 }
 
-verify_cox_phsummary <- function(){
-
+verify_cox_phsummary <- function() {
   hazard_summary <- dsSurvivalClient::ds.coxphSummary(x = 'coxph_serverside')
   expected_names <- c("call", "fail", "na.action", "n", "loglik", "nevent", "coefficients",
                       "conf.int", "logtest", "sctest", "rsq", "waldtest", "used.robust", "concordance")
 
-  if(identical(names(hazard_summary$armadillo), expected_names)){
+  if(identical(names(hazard_summary$armadillo), expected_names)) {
     cli_alert_success("ds.coxphSummary passed")
-  } else{
+  } else {
     cli_alert_danger("ds.coxphSummary failed")
     exit_test("ds.coxphSummary did not create a list with the expected elements")
   }
