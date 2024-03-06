5d4d3ab1374da4705c89d8f2ef2186a8d7872a1e
Tim Cadman <41470917+timcadman@users.noreply.github.com>
2024-03-06 16:51:14 +0100

    refactor: removed files which have now changed director

--- scripts/release/release-test-info.R
+++
@@ -1,9 +0,0 @@
-cat(sprintf("
-                  ,.-----__                       Testing version: %s
-            ,:::://///,:::-.                      Test server: %s
-           /:''/////// ``:::`;/|/                 OIDC User: %s
-          /'   ||||||     :://'`\\                 Admin password set: %s
-        .' ,   ||||||     `/(  e \\                Directory for test files: %s
-  -===~__-'\\__X_`````\\_____/~`-._ `.              Profile: %s
-              ~~        ~~       `~-'             Admin-only mode: %s
-", version, armadillo_url, user, admin_pwd != "", dest, profile, ADMIN_MODE))
