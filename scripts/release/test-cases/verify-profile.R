161518b4305ce6c0a010c26fb3d5a0d0dc3cacb1
Tim Cadman <41470917+timcadman@users.noreply.github.com>
2024-03-06 12:39:20 +0100

    refactor: moved function into relevant script

--- scripts/release/test-cases/verify-profile.R
+++ scripts/release/test-cases/verify-profile.R
@@ -1,3 +1,28 @@
+create_ds_connection <- function(password = "", token = "", profile = "", url) {
+  cli_alert_info("Creating new datashield connection")
+  if (ADMIN_MODE) {
+    cli_alert_info("Creating connection as admin")
+    con <- dsConnect(
+      drv = armadillo(),
+      name = "armadillo",
+      user = "admin",
+      password = password,
+      url = url,
+      profile = profile
+    )
+  } else {
+    cli_alert_info("Creating connection using token")
+    con <- dsConnect(
+      drv = armadillo(),
+      name = "armadillo",
+      token = token,
+      url = url,
+      profile = profile
+    )
+  }
+  return(con)
+}
+
 verify_profile <- function(password, token, url, profile){
 
     con <- create_ds_connection(password = admin_pwd, token = token, url = armadillo_url, profile = profile)
