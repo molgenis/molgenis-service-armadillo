544d8e8ceb1250ba956c3fa0d94320bc8e0af9e6
Tim Cadman <41470917+timcadman@users.noreply.github.com>
2024-03-11 11:52:35 +0100

    refactor: specified arguments

--- scripts/release/test-cases/download-resources.R
+++ scripts/release/test-cases/download-resources.R
@@ -1,4 +1,10 @@
-prepare_resources <- function(rda_dir) {
+prepare_resources <- function(test_file_path, skip_tests) {
+    test_name <- "prepare-resources"
+    if(skip_tests %in% test_name){
+    return(cli_alert_info(sprintf("Test '%s' skipped", test_name)))
+    }
+
+    rda_dir <- file.path(test_file_path, "gse66351_1.rda")
     if(!file.exists(rda_dir)){
       cli_alert_warning("Unable to locate gse66351_1.rda in testing directory, downloading.")
       download.file("https://github.com/isglobal-brge/brge_data_large/raw/master/data/gse66351_1.rda", rda_dir)
@@ -8,4 +14,5 @@ prepare_resources <- function(rda_dir) {
     if (rda_dir == "" || !file.exists(rda_dir)) {
       exit_test(sprintf("File [%s] doesn't exist", rda_dir))
     }
+    cli_alert_success("Resource ready for testing")
 }
