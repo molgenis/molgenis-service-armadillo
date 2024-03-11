a2d1475dde6d1235a3afc6535708b1afeec79052
Tim Cadman <41470917+timcadman@users.noreply.github.com>
2024-03-11 14:09:02 +0100

    refactor: return auth type from config

--- scripts/release/test-cases/test-config.R
+++ scripts/release/test-cases/test-config.R
@@ -144,29 +144,3 @@ configure_test <- function() {
     admin_pwd = admin_pwd, test_file_path = test_file_path, service_location = service_location, dest = dest,
     app_info = app_info, version = version, auth_type = auth_type))
     }
-
-
-
-
-set_admin_or_get_token <- function(skip_tests, url) {
-    test_name <- "set-admin-mode"
-    if(skip_tests %in% test_name){
-    return(cli_alert_info(sprintf("Test '%s' skipped", test_name)))
-    }
-
-    if(ADMIN_MODE){
-      token <- admin_pwd
-      auth_type <- "basic"
-    } else {
-      cli_alert_info("Obtaining TOKEN from '.env.")
-      token <- Sys.getenv("TOKEN")
-      if(token == ""){
-        cli_alert_warning("TOKEN not set, obtaining from armadillo.")
-        token <- armadillo.get_token(url)
-      }
-      auth_type <- "bearer"
-    }
-    return(list(token = token, auth_type = auth_type))
-    cli_alert_success("Permissions set")
-    }
-
