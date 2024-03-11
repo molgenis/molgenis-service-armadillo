fc3234b1bb01faa0d1401e362583ce9e627c3638
Tim Cadman <41470917+timcadman@users.noreply.github.com>
2024-03-11 15:49:30 +0100

    fixed admin password message

--- scripts/release/test-cases/release-test-info.R
+++ scripts/release/test-cases/release-test-info.R
@@ -1,6 +1,6 @@
 show_test_info <- function(version, url, user, admin_pwd, dest, profile, ADMIN_MODE){
     if(admin_pwd != ""){
-    admin_pwd_msg = ""
+    admin_pwd_msg = TRUE
     }
 
     test_message <- cat(sprintf("
@@ -12,13 +12,6 @@ show_test_info <- function(version, url, user, admin_pwd, dest, profile, ADMIN_M
       -===~__-'\\__X_`````\\_____/~`-._ `.              Profile: %s
                   ~~        ~~       `~-'             Admin-only mode: %s
     ", version, url, user, admin_pwd_msg, dest, profile, ADMIN_MODE))
-    print(version)
-    print(url)
-    print(user)
-    print(admin_pwd_msg)
-    print(dest)
-    print(profile)
-    print(ADMIN_MODE)
 
     return(test_message)
 }
