7e7a721d1d6569bb0503d797865e471a3f131499
Tim Cadman <41470917+timcadman@users.noreply.github.com>
2024-03-12 12:09:44 +0100

    refactor: pass additional required arguments

--- scripts/release/test-cases/manual-test.R
+++ scripts/release/test-cases/manual-test.R
@@ -1,15 +1,20 @@
-interactive_test <- function(project1) {
+interactive_test <- function(project1, interactive, skip_tests) {
+    test_name <- "manual-test"
+    if(skip_tests %in% test_name){
+    return(cli_alert_info(sprintf("Test '%s' skipped", test_name)))
+    }
+
     cat("\nNow open your testserver in the browser")
     cat(sprintf("\n\nVerify [%s] is available", project1))
-    wait_for_input()
+    wait_for_input(interactive)
     cat("\nClick on the icon next to the name to go to the project explorer")
-    wait_for_input()
+    wait_for_input(interactive)
     cat("\nVerify the 1_1-outcome-1_0 and 2_1-core-1_0 folders are there")
-    wait_for_input()
+    wait_for_input(interactive)
     cat("\nVerify core contains nonrep, yearlyrep, monthlyrep and trimesterrep")
-    wait_for_input()
+    wait_for_input(interactive)
     cat("\nVerify outcome contains nonrep and yearlyrep")
-    wait_for_input()
+    wait_for_input(interactive)
 
     if (interactive) {
       cat("\nWere the manual tests successful? (y/n) ")
