e34af05de7ecb2f1a1199c4f462d8280ef554e9e
Tim Cadman <41470917+timcadman@users.noreply.github.com>
2024-03-12 17:11:10 +0100

    added all interdepencies

--- scripts/release/test-cases/remove-data.R
+++ scripts/release/test-cases/remove-data.R
@@ -1,14 +1,11 @@
 library(purrr)
 
-set_dm_permissions <- function(user, admin_pwd, required_projects, interactive) {
-print(user)
-print(admin_pwd)
-print(required_projects)
-print(interactive)
+set_dm_permissions <- function(user, admin_pwd, required_projects, interactive, update_auto, url) {
     if(update_auto == "y"){
-      set_user(user, admin_pwd, T, required_projects)
+      set_user(user, admin_pwd, T, required_projects, url)
+      cli_alert_info("Admin reset")
     } else{
-      cat("Make your account admin again")
+      cli_alert_info("Make your account admin again")
       wait_for_input(interactive)
     }
 }
@@ -29,17 +26,17 @@ dm_delete_projects <- function(project, interactive) {
    wait_for_input(interactive)
    }
 
-dm_clean_up <- function(user, admin_pwd, required_projects, interactive, skip_tests) {
+dm_clean_up <- function(user, admin_pwd, required_projects, interactive, update_auto, url, skip_tests) {
      test_name <- "remove-data"
     if(any(skip_tests %in% test_name)) {
     return(cli_alert_info(sprintf("Test '%s' skipped", test_name)))
     }
     cli_alert_info("We're now continuing with the datamanager workflow as admin\n")
     cli_alert_info("Resetting admin permissions")
-    set_dm_permissions(user, admin_pwd, required_projects, interactive)
+    set_dm_permissions(user, admin_pwd, required_projects, interactive, update_auto, url)
     cli_alert_info("Removing tables")
     dm_delete_tables()
     cli_alert_info("Removing projects")
-    map(required_projects, dm_delete_projects(interactive))
+    map(required_projects, ~dm_delete_projects(.x, interactive))
     cli_alert_success("Successfully removed data as admin")
 }
