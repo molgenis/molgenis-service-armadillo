bb39b0e07ad76741117c89476a3983d11c984b68
Tim Cadman <41470917+timcadman@users.noreply.github.com>
2024-03-20 16:20:44 +0100

    chore: remove duplicate section of code

--- scripts/release/test-cases/download-resources.R
+++ scripts/release/test-cases/download-resources.R
@@ -1,9 +1,4 @@
 prepare_resources <- function(resource_path, url, skip_tests) {
-  test_name <- "prepare_resources"
-  if (do_skip_test(test_name, skip_tests)) {
-    return()
-  }
-
   test_name <- "prepare-resources"
   if (any(skip_tests %in% test_name)) {
     return(cli_alert_info(sprintf("Test '%s' skipped", test_name)))
