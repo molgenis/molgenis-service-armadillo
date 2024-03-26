4f3c35b9c7b8f5832171d6372c97b8b1db616644
Clemens Tolboom <c.g.w.m.tolboom@umcg.nl>
2024-03-26 14:06:02 +0100

    UI: Add basic property editor

---
+++ ui/src/components/PropertiesEditor.vue
@@ -0,0 +1,63 @@
+<template>
+  <div>
+    <table class="table">
+      <thead>
+        <tr>
+          <td>Key</td>
+          <td>Title</td>
+          <td>Value</td>
+          <td>Actions</td>
+        </tr>
+      </thead>
+      <tbody v-for="(item, index) in items" :key="index">
+        <tr v-if="editIndex !== index">
+          <td>{{ item.key }}</td>
+          <td>{{ item.title }}</td>
+          <td>{{ item.value }}</td>
+          <td>
+            <button @click="editItem(index)">Edit</button>
+          </td>
+        </tr>
+        <tr v-else>
+          <td>{{ item.key }}</td>
+          <td>{{ item.title }}</td>
+          <td>
+            <input v-model="tempItem.value" />
+          </td>
+          <td>
+            <button @click="cancelEdit">Cancel</button>
+            <button @click="saveItem(index)">Save</button>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      items: [
+        { key: "a.c:", title: "A of B", type: "String", value: "Value" },
+        { key: "a.c", title: "A of C", type: "integer", value: 12 },
+      ],
+      tempItem: null,
+      editIndex: -1,
+    };
+  },
+  methods: {
+    editItem(index) {
+      this.tempItem = { ...this.items[index] };
+      this.editIndex = index;
+    },
+    saveItem(index) {
+      this.items[index] = this.tempItem;
+      this.editIndex = -1;
+    },
+    cancelEdit() {
+      this.editIndex = -1;
+    },
+  },
+};
+</script>
