4060cafa709d8e9c953b08a1cc038abda7c1648c
DickPostma <(ID)+(username)@users.noreply.github.com>
2024-03-27 09:36:26 +0100

    Names

--- ci/create_or_update_k8s-azure.sh
+++ ci/create_or_update_k8s-azure.sh
@@ -25,13 +25,13 @@ echo ${CERTDEVMOLGENIS_PEM} | base64 --decode >> /tmp/cert_pem
 # wait for deletion to complete
 sleep 15s
 kubectl create namespace $NAME
-kubectl create secret tls "dev.molgenis.org" --key /tmp/cert_key --cert /tmp/cert_pem -n armadillo-${NAME}
+kubectl create secret tls "dev.molgenis.org" --key /tmp/cert_key --cert /tmp/cert_pem -n ${NAME}
 
-helm upgrade --install armadillo-${NAME} ./helm-chart --namespace armadillo-${NAME} \
+helm upgrade --install ${NAME} ./helm-chart --namespace ${NAME} \
 --set ingress.hosts[0].paths[0].path=/ \
 --set ingress.hosts[0].paths[0].pathType=ImplementationSpecific \
---set ingress.hosts[0].host=armadillo-${NAME}.dev.molgenis.org \
---set ingress.tls[0].host=armadillo-${NAME}.dev.molgenis.org \
+--set ingress.hosts[0].host=${NAME}.dev.molgenis.org \
+--set ingress.tls[0].host=${NAME}.dev.molgenis.org \
 --set ingress.tls[0].secretName=dev.molgenis.org \
 --set adminPassword=adminArmadillo! \
 --set image.tag=${TAG_NAME} \
