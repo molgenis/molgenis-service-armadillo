3d880cb3ee9c7976ff558e0f246bffe98d65c88b
Clemens Tolboom <c.g.w.m.tolboom@umcg.nl>
2024-04-09 11:09:06 +0200

    Add logging for restarting the app

--- armadillo/src/main/java/org/molgenis/armadillo/ArmadilloServiceApplication.java
+++ armadillo/src/main/java/org/molgenis/armadillo/ArmadilloServiceApplication.java
@@ -1,5 +1,7 @@
 package org.molgenis.armadillo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,13 +11,16 @@ import org.springframework.retry.annotation.EnableRetry;
 @SpringBootApplication(scanBasePackages = "org.molgenis")
 @EnableRetry
 public class ArmadilloServiceApplication {
+  private static final Logger LOGGER = LoggerFactory.getLogger(ArmadilloServiceApplication.class);
   private static ConfigurableApplicationContext context;
 
   public static void main(String[] args) {
+    LOGGER.info("Application starting");
     context = SpringApplication.run(ArmadilloServiceApplication.class, args);
   }
 
   public static void restart() {
+    LOGGER.info("Application restarting");
     ApplicationArguments args = context.getBean(ApplicationArguments.class);
     Thread thread =
         new Thread(
