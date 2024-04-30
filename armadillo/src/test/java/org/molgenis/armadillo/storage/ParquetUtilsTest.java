c2fdb2e89d07af63cc7f822232e9418f63b5f587
mkslofstra <mariska-karina@hotmail.com>
2024-04-30 09:12:32 +0200

    test(ParquetUtils): add test for variables

--- armadillo/src/test/java/org/molgenis/armadillo/storage/ParquetUtilsTest.java
+++ armadillo/src/test/java/org/molgenis/armadillo/storage/ParquetUtilsTest.java
@@ -1,6 +1,6 @@
 package org.molgenis.armadillo.storage;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -8,19 +8,27 @@ import java.net.URISyntaxException;
 import java.nio.file.Path;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import org.junit.jupiter.api.Test;
 
 public class ParquetUtilsTest {
   @Test
   public void testParquetPreview() throws IOException, URISyntaxException {
-    Path path = Path.of(this.getClass().getClassLoader().getResource("patient.parquet").toURI());
+    Path path =
+        Path.of(
+            Objects.requireNonNull(this.getClass().getClassLoader().getResource("patient.parquet"))
+                .toURI());
     List<Map<String, String>> preview = ParquetUtils.previewRecords(path, 10, 10, new String[0]);
     assertEquals("Patient1", preview.get(0).get("name"));
+    assertEquals(preview.size(), 10);
   }
 
   @Test
   void testRetrieveDimensions() throws URISyntaxException, FileNotFoundException {
-    Path path = Path.of(this.getClass().getClassLoader().getResource("patient.parquet").toURI());
+    Path path =
+        Path.of(
+            Objects.requireNonNull(this.getClass().getClassLoader().getResource("patient.parquet"))
+                .toURI());
     Map<String, String> dimensions = ParquetUtils.retrieveDimensions(path);
     assertEquals("3", dimensions.get("columns"));
     assertEquals("11", dimensions.get("rows"));
@@ -28,7 +36,22 @@ public class ParquetUtilsTest {
 
   @Test
   void testGetColumns() throws URISyntaxException, IOException {
-    Path path = Path.of(this.getClass().getClassLoader().getResource("patient.parquet").toURI());
+    Path path =
+        Path.of(
+            Objects.requireNonNull(this.getClass().getClassLoader().getResource("patient.parquet"))
+                .toURI());
     assertEquals(List.of("id", "age", "name"), ParquetUtils.getColumns(path));
+  }
+
+  @Test
+  void testPreviewWithVariables() throws URISyntaxException, IOException {
+    Path path =
+        Path.of(
+            Objects.requireNonNull(this.getClass().getClassLoader().getResource("patient.parquet"))
+                .toURI());
+    List<Map<String, String>> preview =
+        ParquetUtils.previewRecords(path, 10, 10, new String[] {"age"});
+    assertEquals("24", preview.get(0).get("age"));
+    assertNull(preview.get(0).get("name"));
   }
 }
