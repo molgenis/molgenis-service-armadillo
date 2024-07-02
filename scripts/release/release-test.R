fb8ab31f9735ff56c42325dde30ed0ce59765a66
Tim Cadman <41470917+timcadman@users.noreply.github.com>
2024-07-02 15:27:20 +0200

    try: add install to release script

--- scripts/release/release-test.R
+++ scripts/release/release-test.R
@@ -19,6 +19,7 @@ print(sessionInfo())
 library(cli)
 cli_h1("Setup")
 cli_alert_info("Loading libraries")
+install.packages("DSMolgenisArmadillo")
 # for password prompt
 library(getPass)
 # for reading parquet files
@@ -139,18 +140,18 @@ verify_ds_mtl(skip_tests = test_config$skip_tests)
 
 cli_alert_info("Testing dsExposome")
 source("test-cases/xenon-exposome.R")
-run_exposome_tests(project = project1, url = test_config$armadillo_url, token = token, auth_type = test_config$auth_type, 
-                   ADMIN_MODE = test_config$ADMIN_MODE, profile = test_config$profile, profile_info = profile_info, 
-                   ref = exposome_ref, skip_tests = test_config$skip_tests, 
-                   user = test_config$user, admin_pwd = test_config$admin_pwd, interactive = test_config$interactive, 
+run_exposome_tests(project = project1, url = test_config$armadillo_url, token = token, auth_type = test_config$auth_type,
+                   ADMIN_MODE = test_config$ADMIN_MODE, profile = test_config$profile, profile_info = profile_info,
+                   ref = exposome_ref, skip_tests = test_config$skip_tests,
+                   user = test_config$user, admin_pwd = test_config$admin_pwd, interactive = test_config$interactive,
                    update_auto = test_config$update_auto)
 
 cli_alert_info("Testing dsOmics")
 source("test-cases/xenon-omics.R")
-run_omics_tests(project = project1, url = test_config$armadillo_url, token = token, auth_type = test_config$auth_type, 
-                   ADMIN_MODE = test_config$ADMIN_MODE, profile = test_config$profile, profile_info = profile_info, 
-                   ref = omics_ref, skip_tests = test_config$skip_tests, 
-                   user = test_config$user, admin_pwd = test_config$admin_pwd, interactive = test_config$interactive, 
+run_omics_tests(project = project1, url = test_config$armadillo_url, token = token, auth_type = test_config$auth_type,
+                   ADMIN_MODE = test_config$ADMIN_MODE, profile = test_config$profile, profile_info = profile_info,
+                   ref = omics_ref, skip_tests = test_config$skip_tests,
+                   user = test_config$user, admin_pwd = test_config$admin_pwd, interactive = test_config$interactive,
                    update_auto = test_config$update_auto)
 
 cli_h2("Removing data as admin")
