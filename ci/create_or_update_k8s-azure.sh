c90023b279cdeed8479edaa4b7b7ac29e4d9fce1
DickPostma <(ID)+(username)@users.noreply.github.com>
2024-10-21 12:07:49 +0200

    Feat(Add OIDC preview integration)

--- ci/create_or_update_k8s-azure.sh
+++ ci/create_or_update_k8s-azure.sh
@@ -30,7 +30,7 @@ helm upgrade --install ${NAME} ./helm-chart --namespace ${NAME} \
 --set ingress.hosts[0].host=${NAME}.dev.molgenis.org \
 --set ingress.tls[0].host=${NAME}.dev.molgenis.org \
 --set ingress.tls[0].secretName=dev.molgenis.org \
---set config.security.user.password=${ADMINPASS} \
+--set config.spring.security.user.password=${ADMINPASS} \
 #--set config.armadillo.oauth2.client.registration.molgenis.redirect-uri=${NAME}.dev.molgenis.org/login/oauth2/code/molgenis \
 #--set config.armadillo.oauth2.client.registration.molgenis.client-id=${OIDC_CLIENTID} \
 #--set config.armadillo.oauth2.client.registration.molgenis.client-secret=${OIDC_CLIENTSECRET} \
