4551d7871532835f03fc9deb19e29dcc406a09c0
Tim Cadman <41470917+timcadman@users.noreply.github.com>
2025-01-20 11:02:17 +0100

    chore: added workspace endpoint to routing

--- ui/vite.config.js
+++ ui/vite.config.js
@@ -95,6 +95,13 @@ export default defineConfig({
           port: 8080,
         },
       },
+      "^/all-workspaces": {
+        target: {
+          protocol: "http:",
+          host: "localhost",
+          port: 8080,
+        },
+      },
     },
   },
 });
