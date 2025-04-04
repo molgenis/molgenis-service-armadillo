85eb796a7a9806311260579a722b15d8278fd7e7
mkslofstra <mariska-karina@hotmail.com>
2025-04-04 14:42:30 +0200

    test: make test with anonymous user

--- armadillo/src/test/java/org/molgenis/armadillo/controller/ProfilesControllerTest.java
+++ armadillo/src/test/java/org/molgenis/armadillo/controller/ProfilesControllerTest.java
@@ -29,6 +29,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
+import org.springframework.security.test.context.support.WithAnonymousUser;
 import org.springframework.security.test.context.support.WithMockUser;
 
 @WebMvcTest(ProfilesController.class)
@@ -149,7 +150,7 @@ class ProfilesControllerTest extends ArmadilloControllerTestBase {
   }
 
   @Test
-  @WithMockUser(roles = "SU")
+  @WithAnonymousUser
   void getProfileStatus_GET() throws Exception {
     ContainerInfo runningContainer = ContainerInfo.create(ProfileStatus.RUNNING);
     ContainerInfo offlineContainer = ContainerInfo.create(ProfileStatus.DOCKER_OFFLINE);
