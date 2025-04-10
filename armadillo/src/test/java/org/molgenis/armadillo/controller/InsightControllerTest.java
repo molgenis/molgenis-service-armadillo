d779b98e74027b2c197f9cd50613be1585eedac6
mkslofstra <mariska-karina@hotmail.com>
2025-04-10 13:08:52 +0200

    chore: remove unused code

--- armadillo/src/test/java/org/molgenis/armadillo/controller/InsightControllerTest.java
+++ armadillo/src/test/java/org/molgenis/armadillo/controller/InsightControllerTest.java
@@ -4,7 +4,6 @@ import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -36,11 +35,6 @@ public class InsightControllerTest {
 
   @MockitoBean ArmadilloStorageService armadilloStorage;
   @Autowired AccessService accessService;
-
-  @BeforeEach
-  public void before() {
-    //      runAsSystem(() -> insightService.initialize());
-  }
 
   @Test
   public void testFilesList() throws Exception {
