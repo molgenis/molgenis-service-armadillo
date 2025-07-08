3f5288a3c99c21567be979151032e9a41ace367c
Tim Cadman <41470917+timcadman@users.noreply.github.com>
2025-07-08 13:06:27 +0200

    chore: no longer using Scheduled

--- armadillo/src/main/java/org/molgenis/armadillo/ArmadilloServiceApplication.java
+++ armadillo/src/main/java/org/molgenis/armadillo/ArmadilloServiceApplication.java
@@ -3,11 +3,9 @@ package org.molgenis.armadillo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.retry.annotation.EnableRetry;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication(scanBasePackages = "org.molgenis")
 @EnableRetry
-@EnableScheduling
 public class ArmadilloServiceApplication {
 
   public static void main(String[] args) {
