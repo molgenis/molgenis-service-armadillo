980a2f51c93bd06cf48cb10b6167ec2947c3b4ff
Tim Cadman <41470917+timcadman@users.noreply.github.com>
2025-08-29 12:34:26 +0200

    corrected endpoint address

--- armadillo/src/main/java/org/molgenis/armadillo/controller/ProfileStartStatusController.java
+++ armadillo/src/main/java/org/molgenis/armadillo/controller/ProfileStartStatusController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/profiles")
+@RequestMapping("/ds-profiles")
 public class ProfileStartStatusController {
 
   private final ProfileStatusService statusService;
