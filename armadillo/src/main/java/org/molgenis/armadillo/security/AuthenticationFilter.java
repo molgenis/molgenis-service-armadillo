8847bf375628930c7178ebacee04bca18d0916d3
mkslofstra <mariska-karina@hotmail.com>
2025-10-31 15:47:55 +0100

    chore: comment out headers

--- armadillo/src/main/java/org/molgenis/armadillo/security/AuthenticationFilter.java
+++ armadillo/src/main/java/org/molgenis/armadillo/security/AuthenticationFilter.java
@@ -6,7 +6,6 @@ import jakarta.servlet.ServletResponse;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
-import java.util.Enumeration;
 import org.springframework.http.MediaType;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -14,35 +13,35 @@ import org.springframework.web.filter.GenericFilterBean;
 
 public class AuthenticationFilter extends GenericFilterBean {
 
-    private String AUTH_TOKEN;
+  private String AUTH_TOKEN;
 
-    public void setAuthToken(String authToken) {
-        AUTH_TOKEN = authToken;
-    }
+  public void setAuthToken(String authToken) {
+    AUTH_TOKEN = authToken;
+  }
 
-    @Override
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain filterChain) {
-        try {
-            if (request instanceof HttpServletRequest httpRequest) {
-                Enumeration<String> headers = httpRequest.getHeaderNames();
-                String URI = httpRequest.getRequestURI();
+  @Override
+  public void doFilter(ServletRequest request, ServletResponse response, FilterChain filterChain) {
+    try {
+      if (request instanceof HttpServletRequest httpRequest) {
+        //        Enumeration<String> headers = httpRequest.getHeaderNames();
+        String URI = httpRequest.getRequestURI();
 
-                if (URI.startsWith("/actuator/")
-                        && !URI.equals("/actuator/info")
-                        && !URI.equals("/actuator/health")) {
-                    Authentication authentication =
-                            AuthenticationService.getAuthentication((HttpServletRequest) request, AUTH_TOKEN);
-                    SecurityContextHolder.getContext().setAuthentication(authentication);
-                }
-                HttpSession session = httpRequest.getSession(true);
-                session.setAttribute("SPRING_SECURITY_CONTEXT", SecurityContextHolder.getContext());
-                filterChain.doFilter(request, response);
-            }
-        } catch (Exception exp) {
-            HttpServletResponse httpResponse = (HttpServletResponse) response;
-            httpResponse.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-            httpResponse.setContentType(MediaType.APPLICATION_JSON_VALUE);
-            this.logger.error(exp.getMessage(), exp);
+        if (URI.startsWith("/actuator/")
+            && !URI.equals("/actuator/info")
+            && !URI.equals("/actuator/health")) {
+          Authentication authentication =
+              AuthenticationService.getAuthentication((HttpServletRequest) request, AUTH_TOKEN);
+          SecurityContextHolder.getContext().setAuthentication(authentication);
         }
+        HttpSession session = httpRequest.getSession(true);
+        session.setAttribute("SPRING_SECURITY_CONTEXT", SecurityContextHolder.getContext());
+        filterChain.doFilter(request, response);
+      }
+    } catch (Exception exp) {
+      HttpServletResponse httpResponse = (HttpServletResponse) response;
+      httpResponse.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+      httpResponse.setContentType(MediaType.APPLICATION_JSON_VALUE);
+      this.logger.error(exp.getMessage(), exp);
     }
+  }
 }
