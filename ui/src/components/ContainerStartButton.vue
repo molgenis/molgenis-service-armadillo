d4c4f47d1d9683dbfdfb6577a5d9e9238c6df694
mkslofstra <mariska-karina@hotmail.com>
2025-12-09 16:26:06 +0100

    fix: connect extra properties from containers to show RL data
    todo:
    - extract more components
    - add button
    - edit button
    - tests

---
+++ ui/src/components/ContainerStartButton.vue
@@ -0,0 +1,52 @@
+<template>
+  <button
+    class="btn p-0 btn-link"
+    :class="`${status.text === 'Start' ? 'text-success' : 'text-danger'}`"
+    @click="status.text === 'Start' ? startFunction(name) : stopFunction(name)"
+    :disabled="isDisabled"
+  >
+    <LoadingSpinner v-if="isLoading" imageWidth="40" class="m-0 mt-1 p-0" />
+    <h2 class="ms-2 mt-1 mb-1 me-0 p-0" v-else>
+      <i :class="`bi bi-${status.icon}`"></i>
+    </h2>
+  </button>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from "vue";
+import LoadingSpinner from "@/components/LoadingSpinner.vue";
+import { StatusMappingType } from "@/types/types";
+
+export default defineComponent({
+  name: "ContainerStartButton",
+  components: { LoadingSpinner },
+  props: {
+    status: {
+      type: Object as PropType<StatusMappingType>,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    startFunction: {
+      type: Function,
+      required: true,
+    },
+    stopFunction: {
+      type: Function,
+      required: true,
+    },
+    isDisabled: {
+      type: Boolean,
+      required: true,
+    },
+    isLoading: {
+      type: Boolean,
+      required: true,
+    },
+  },
+});
+</script>
+
+<style scoped></style>
---
+++ ui/src/components/ContainerTypeLogo.vue
@@ -0,0 +1,40 @@
+<template>
+  <span>
+    <DsLogo :height="25" class="mt-2" v-if="template === 'ds'" />
+    <FlowerLogo
+      class="position-absolute top-50 start-50 translate-middle"
+      :height="20"
+      v-else-if="template.includes('flwr') || name.startsWith('flower')"
+    />
+    <DockerLogo
+      class="position-absolute top-50 start-50 translate-middle"
+      :height="25"
+      v-else
+    />
+  </span>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from "vue";
+import DockerLogo from "@/components/DockerLogo.vue";
+import FlowerLogo from "@/components/FlowerLogo.vue";
+import DsLogo from "@/components/DsLogo.vue";
+import { ContainerType } from "@/types/types";
+
+export default defineComponent({
+  name: "ContainerTypeLogo",
+  components: { DsLogo, FlowerLogo, DockerLogo },
+  props: {
+    template: {
+      type: String as PropType<ContainerType>,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+  },
+});
+</script>
+
+<style scoped></style>
