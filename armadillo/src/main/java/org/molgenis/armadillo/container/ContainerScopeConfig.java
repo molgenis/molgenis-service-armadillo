f0a5e7185ad7b50309f350f77eda6e1433a78914
Tim Cadman <41470917+timcadman@users.noreply.github.com>
2025-12-12 15:52:18 +0100

    decoupled container scope config from datashield

--- armadillo/src/main/java/org/molgenis/armadillo/container/ContainerScopeConfig.java
+++ armadillo/src/main/java/org/molgenis/armadillo/container/ContainerScopeConfig.java
@@ -7,7 +7,6 @@ import static org.molgenis.armadillo.security.RunAs.runAsSystem;
 import org.molgenis.armadillo.exceptions.UnknownContainerException;
 import org.molgenis.armadillo.metadata.ContainerService;
 import org.molgenis.r.RConnectionFactory;
-import org.molgenis.r.RServerConnectionFactory;
 import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -17,11 +16,10 @@ public class ContainerScopeConfig {
 
   @Bean
   @org.molgenis.armadillo.container.annotation.ContainerScope
-  public DatashieldContainerConfig containerConfig(ContainerService containerService) {
+  public ContainerConfig containerConfig(ContainerService containerService) {
     var activeContainerName = getActiveContainerName();
     try {
-      return runAsSystem(
-          () -> (DatashieldContainerConfig) containerService.getByName(activeContainerName));
+      return runAsSystem(() -> containerService.getByName(activeContainerName));
     } catch (UnknownContainerException e) {
       throw new IllegalStateException(
           format(
@@ -32,8 +30,15 @@ public class ContainerScopeConfig {
   @Bean
   @org.molgenis.armadillo.container.annotation.ContainerScope
   public RConnectionFactory rConnectionFactory(
-      DatashieldContainerConfig datashieldContainerConfig) {
-    return new RServerConnectionFactory(datashieldContainerConfig.toEnvironmentConfigProps());
+      ContainerConfig ContainerConfig, DatashieldRConnectionFactoryProvider datashieldProvider) {
+
+    if (ContainerConfig instanceof DatashieldContainerConfig datashieldConfig) {
+      return datashieldProvider.create(datashieldConfig);
+    }
+    throw new UnsupportedOperationException(
+        format(
+            "Container type '%s' does not support R connections. The RConnectionFactory is only available for DataSHIELD containers.",
+            ContainerConfig.getClass().getSimpleName()));
   }
 
   @Bean
