a6b637d2fe9a6f8ff07d6f474e1fca9c9fb983db
Tim Cadman <41470917+timcadman@users.noreply.github.com>
2025-12-16 14:47:38 +0100

    further simplify

--- armadillo/src/main/java/org/molgenis/armadillo/container/UpdatableContainerConfig.java
+++ armadillo/src/main/java/org/molgenis/armadillo/container/UpdatableContainerConfig.java
@@ -1,9 +1,15 @@
 package org.molgenis.armadillo.container;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.annotation.Nullable;
 import org.molgenis.armadillo.metadata.UpdateSchedule;
 
 public interface UpdatableContainerConfig {
+  @JsonProperty("autoUpdate")
+  @Nullable
   Boolean getAutoUpdate();
 
+  @JsonProperty("updateSchedule")
+  @Nullable
   UpdateSchedule getUpdateSchedule();
 }
