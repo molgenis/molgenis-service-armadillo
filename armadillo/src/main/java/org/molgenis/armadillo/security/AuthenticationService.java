79421c8567cb9138731eaa96ea8e032f48e8e3c2
mkslofstra <mariska-karina@hotmail.com>
2026-01-09 16:27:37 +0100

    does this do the trick?

--- armadillo/src/main/java/org/molgenis/armadillo/security/AuthenticationService.java
+++ armadillo/src/main/java/org/molgenis/armadillo/security/AuthenticationService.java
@@ -7,38 +7,53 @@ import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.core.context.SecurityContextImpl;
+import org.springframework.util.StringUtils;
 
 public class AuthenticationService {
 
   private static final String AUTH_TOKEN_HEADER_NAME = "X-API-KEY";
 
   public static Authentication getAuthentication(HttpServletRequest request, String authToken) {
-    String apiKey = request.getHeader(AUTH_TOKEN_HEADER_NAME);
-    String authHeader = request.getHeader("Authorization");
+
+    String authHeader =
+        StringUtils.hasText(request.getHeader("Authorization"))
+            ? request.getHeader("Authorization")
+            : null;
+
+    String apiKey =
+        StringUtils.hasText(request.getHeader(AUTH_TOKEN_HEADER_NAME))
+            ? request.getHeader(AUTH_TOKEN_HEADER_NAME)
+            : null;
+
     HttpSession session = request.getSession(false);
     SecurityContextImpl context =
         session != null
             ? (SecurityContextImpl) session.getAttribute("SPRING_SECURITY_CONTEXT")
             : null;
+
     if (authHeader != null
         && (authHeader.startsWith("Bearer ") || authHeader.startsWith("Basic "))) {
+
       String token = authHeader.substring(authHeader.indexOf(' ') + 1);
       return new KeyAuthentication(authHeader, AuthorityUtils.NO_AUTHORITIES, token);
     }
+
     if (context != null
         && context.getAuthentication() != null
-        && context.getAuthentication().getPrincipal() != null
-        && !(context.getAuthentication() instanceof AnonymousAuthenticationToken)
-        && context.getAuthentication().isAuthenticated()) {
+        && context.getAuthentication().isAuthenticated()
+        && !(context.getAuthentication() instanceof AnonymousAuthenticationToken)) {
+
       return new KeyAuthentication(
           context.getAuthentication().getCredentials(),
           context.getAuthentication().getAuthorities(),
           context.getAuthentication().getPrincipal());
     }
+
     if (apiKey != null && apiKey.equals(authToken)) {
       return new KeyAuthentication(
           apiKey, AuthorityUtils.NO_AUTHORITIES, (Principal) () -> "API_KEY");
     }
+
     return null;
   }
 }
