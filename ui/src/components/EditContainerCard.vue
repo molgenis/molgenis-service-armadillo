07dd05b5cb58636d7c1d28f68c19f62d345aa775
mkslofstra <mariska-karina@hotmail.com>
2026-01-12 14:36:41 +0100

    show arrays differently

--- ui/src/components/EditContainerCard.vue
+++ ui/src/components/EditContainerCard.vue
@@ -4,16 +4,28 @@
     <div class="card-body">
       <form>
         <div class="mb-3" v-for="(value, key) in container">
+          <input
+            class="form-check-input"
+            type="checkbox"
+            value=""
+            v-if="getValueType(value) === 'boolean'"
+          />&nbsp;
           <label for="containerName" class="form-label">{{
             toCapitalizedWords(key)
           }}</label>
+
           <input
-            v-if="typeof value === 'string'"
+            v-if="getValueType(value) === 'string'"
             type="text"
             class="form-control"
             :value="value"
           />
-          {{ typeof value }}
+          <span v-else-if="getValueType(value) === 'array'">
+            <BadgeList :item-array="value" />
+          </span>
+
+          {{ getValueType(value) }}
+          {{ value }}
         </div>
       </form>
       <button class="btn btn-danger"><i class="bi bi-x-lg"></i> Cancel</button>
@@ -27,13 +39,29 @@
 <script lang="ts">
 import { defineComponent, PropType } from "vue";
 import { toCapitalizedWords } from "@/helpers/utils";
+import Badge from "@/components/Badge.vue";
+import BadgeList from "@/components/BadgeList.vue";
 
 export default defineComponent({
   name: "EditContainerCard",
-  methods: { toCapitalizedWords },
+  components: { BadgeList, Badge },
+  methods: {
+    toCapitalizedWords,
+    getValueType(value) {
+      let type = typeof value;
+      if (
+        type === "object" &&
+        Object.prototype.toString.call(value) === "[object Array]"
+      ) {
+        return "array";
+      } else {
+        return type;
+      }
+    },
+  },
   data() {
     return {
-      dontShow: ["imageSize", "installDate"],
+      dontShow: ["imageSize", "installDate", "lastImageId"],
       icons: {
         port: "",
         image: "",
