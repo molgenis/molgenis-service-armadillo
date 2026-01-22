983ac5547104687a44528332331efb178eedb049
Tim Cadman <41470917+timcadman@users.noreply.github.com>
2026-01-22 14:59:41 +0100

    removed code comments

--- armadillo/src/main/java/org/molgenis/armadillo/container/ContainerConfig.java
+++ armadillo/src/main/java/org/molgenis/armadillo/container/ContainerConfig.java
@@ -1,6 +1,5 @@
 package org.molgenis.armadillo.container;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonSubTypes;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import jakarta.annotation.Nullable;
@@ -14,39 +13,26 @@ import java.util.Map;
 })
 public interface ContainerConfig {
 
-  @JsonProperty("name")
-  @Nullable
   String getName();
 
-  @JsonProperty("image")
-  @Nullable
   String getImage();
 
-  @JsonProperty("host")
-  @Nullable
   String getHost();
 
-  @JsonProperty("port")
-  @Nullable
   Integer getPort();
 
-  @JsonProperty("imageSize")
   @Nullable
   Long getImageSize();
 
-  @JsonProperty("installDate")
   @Nullable
   String getInstallDate();
 
-  @JsonProperty("lastImageId")
   @Nullable
   String getLastImageId();
 
-  @JsonProperty("dockerArgs")
   @Nullable
   List<String> getDockerArgs();
 
-  @JsonProperty("dockerOptions")
   @Nullable
   Map<String, Object> getDockerOptions();
 
