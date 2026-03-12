7cda24c495fa27b5687c5a4a635f9d8151f418af
mkslofstra <mariska-karina@hotmail.com>
2026-03-12 07:39:07 +0100

    feat: copy and visibility for client secret

--- ui/src/components/OidcConfig.vue
+++ ui/src/components/OidcConfig.vue
@@ -12,9 +12,9 @@
     </h5>
     <div class="card-body">
       <div class="mb-3 row">
-        <label for="inputServer" class="col-sm-2 col-form-label"
-          ><i class="bi bi-database-fill"></i> Server URL</label
-        >
+        <label for="inputServer" class="col-sm-2 col-form-label">
+          <i class="bi bi-database-fill"></i> Server URL
+        </label>
         <div class="col-sm-10">
           <input
             type="text"
@@ -27,9 +27,9 @@
         </div>
       </div>
       <div class="mb-3 row">
-        <label for="inputClient" class="col-sm-2 col-form-label"
-          ><i class="bi bi-fingerprint"></i> Client ID</label
-        >
+        <label for="inputClient" class="col-sm-2 col-form-label">
+          <i class="bi bi-fingerprint"></i> Client ID
+        </label>
         <div class="col-sm-10">
           <input
             type="text"
@@ -42,18 +42,42 @@
         </div>
       </div>
       <div class="mb-3 row">
-        <label for="inputSecret" class="col-sm-2 col-form-label"
-          ><i class="bi bi-incognito"></i> Client Secret</label
-        >
+        <label for="inputSecret" class="col-sm-2 col-form-label">
+          <i class="bi bi-incognito"></i> Client Secret
+        </label>
         <div class="col-sm-10">
-          <input
-            type="text"
-            class="form-control"
-            id="inputSecret"
-            v-model="clientSecret"
-            :placeholder="clientSecret"
-            :disabled="!isEditMode"
-          />
+          <div class="input-group">
+            <input
+              :type="showSecret ? 'text' : 'password'"
+              class="form-control"
+              id="inputSecret"
+              v-model="clientSecret"
+              :placeholder="clientSecret"
+              :disabled="!isEditMode"
+            />
+            <!-- toggle visibility here -->
+            <button
+              class="btn btn-info"
+              type="button"
+              @click="toggleVisibilityClientSecret"
+            >
+              <i class="bi bi-eye-slash-fill" v-if="showSecret"></i
+              ><i class="bi bi-eye-fill" v-else></i>
+            </button>
+            <!-- copy here -->
+            <button
+              class="btn"
+              :class="isCopied['secret'] ? 'btn-success' : 'btn-secondary'"
+              type="button"
+              @click="copy(clientSecret, 'secret')"
+            >
+              <i
+                class="bi bi-clipboard-check-fill"
+                v-if="isCopied['secret']"
+              ></i
+              ><i class="bi bi-clipboard-fill" v-else></i>
+            </button>
+          </div>
         </div>
       </div>
       <button class="btn btn-danger" v-if="isEditMode" @click="cancelEdit">
@@ -85,8 +109,25 @@ export default defineComponent({
       required: true,
     },
   },
+  data() {
+    return {
+      showSecret: false,
+      isCopied: {
+        secret: false,
+        id: false,
+        uri: false,
+      },
+      serverUri: this.presetServerUri,
+      clientId: this.presetClientId,
+      clientSecret: this.presetClientSecret,
+      isEditMode: false,
+    };
+  },
   emits: ["saveOidcConfig"],
   methods: {
+    toggleVisibilityClientSecret() {
+      this.showSecret = !this.showSecret;
+    },
     turnOffEditmode() {
       this.isEditMode = false;
     },
@@ -99,6 +140,30 @@ export default defineComponent({
       this.clientId = this.presetClientId;
       this.clientSecret = this.presetClientSecret;
     },
+    copy(variableToCopy: string, typeToCopy: "id" | "secret" | "uri") {
+      const textBlob = new Blob([variableToCopy], { type: "text/plain" });
+      const clipboardItemData: {
+        [mimeType: string]: Blob | string | Promise<Blob | string>;
+      } = {
+        "text/plain": textBlob,
+      };
+
+      const clipboardItem = new ClipboardItem(clipboardItemData);
+
+      navigator.clipboard
+        .write([clipboardItem])
+        .then(() => {
+          // otherwise this is the context of the then function we're in, rather than our data prop
+          const self = this;
+          this.isCopied[typeToCopy] = true;
+          setTimeout(function () {
+            self.isCopied[typeToCopy] = false;
+          }, 1000);
+        })
+        .catch((err) => {
+          console.error("Error copying to clipboard:", err);
+        });
+    },
     triggerSave() {
       this.turnOffEditmode();
       this.$emit("saveOidcConfig", {
@@ -107,14 +172,6 @@ export default defineComponent({
         clientSecret: this.clientSecret,
       });
     },
-  },
-  data() {
-    return {
-      serverUri: this.presetServerUri,
-      clientId: this.presetClientId,
-      clientSecret: this.presetClientSecret,
-      isEditMode: false,
-    };
   },
 });
 </script>
