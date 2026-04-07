1688fcd2ddb752a51ff7c3719dd533191a727d78
mkslofstra <mariska-karina@hotmail.com>
2026-04-07 14:31:56 +0200

    feat: add restart endpoint

--- armadillo/src/main/java/org/molgenis/armadillo/ArmadilloServiceApplication.java
+++ armadillo/src/main/java/org/molgenis/armadillo/ArmadilloServiceApplication.java
@@ -1,14 +1,32 @@
 package org.molgenis.armadillo;
 
+import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.retry.annotation.EnableRetry;
 
 @SpringBootApplication(scanBasePackages = "org.molgenis")
 @EnableRetry
 public class ArmadilloServiceApplication {
+  private static ConfigurableApplicationContext context;
 
   public static void main(String[] args) {
-    SpringApplication.run(ArmadilloServiceApplication.class, args);
+    context = SpringApplication.run(ArmadilloServiceApplication.class, args);
+  }
+
+  public static void restart() {
+    ApplicationArguments args = context.getBean(ApplicationArguments.class);
+
+    Thread thread =
+        new Thread(
+            () -> {
+              context.close();
+              context =
+                  SpringApplication.run(ArmadilloServiceApplication.class, args.getSourceArgs());
+            });
+
+    thread.setDaemon(false);
+    thread.start();
   }
 }
