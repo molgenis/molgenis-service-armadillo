70649ba6b8a8a8307416a79ecdc7bc24f1a1eea4
mkslofstra <mariska-karina@hotmail.com>
2026-04-17 12:56:58 +0200

    feat: delete jars

--- armadillo/src/main/java/org/molgenis/armadillo/service/ManagementService.java
+++ armadillo/src/main/java/org/molgenis/armadillo/service/ManagementService.java
@@ -9,8 +9,6 @@ import com.google.gson.JsonParser;
 import java.io.*;
 import java.net.ProxySelector;
 import java.net.URI;
-import java.net.URL;
-import java.net.URLConnection;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
@@ -28,6 +26,7 @@ import java.util.stream.Stream;
 import java.util.stream.StreamSupport;
 import org.apache.commons.io.FileUtils;
 import org.molgenis.armadillo.ArmadilloServiceApplication;
+import org.molgenis.armadillo.exceptions.StorageException;
 import org.molgenis.armadillo.metadata.OidcDetails;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
@@ -57,6 +56,7 @@ public class ManagementService {
       "https://raw.githubusercontent.com/molgenis/molgenis-service-armadillo/refs/tags/%s/scripts/install/%s";
   String RELEASE_DOWNLOAD_URL =
       "https://github.com/molgenis/molgenis-service-armadillo/releases/download/%s/%s";
+  String ARMADILLO_JAR = "molgenis-armadillo-%s.jar";
   String PRERELEASE = "prerelease";
   String FALSE = "false";
   String PUBLISHED_AT = "published_at";
@@ -233,6 +233,21 @@ public class ManagementService {
     return line.split(":")[0] + ": " + newValue;
   }
 
+  public void deleteJar(String version) {
+    File armadilloJar = new File(armadilloHome + "/" + getJarFromVersion(version));
+    // todo: make sure it's not the jar we're using
+    if (armadilloJar.delete()) {
+      // todo: log
+      System.out.println("Deleted the file: " + armadilloJar.getName());
+    } else {
+      throw new StorageException("Cannot delete file: " + armadilloJar.getName());
+    }
+  }
+
+  private String getJarFromVersion(String version) {
+    return String.format(ARMADILLO_JAR, version.replace("v", ""));
+  }
+
   // Simple mutable state carrier — private inner class or a record (Java 16+)
   private static class ConfigParseState {
     boolean providerFound, providerMolgenisFound;
@@ -245,7 +260,7 @@ public class ManagementService {
   public boolean isArmadilloUpdateAvailable() throws IOException, InterruptedException {
     JsonElement lastRelease = getLastRelease();
     String lastVersion = getLastReleaseVersion(lastRelease);
-    String armadilloJar = String.format("molgenis-armadillo-%s.jar", lastVersion.replace("v", ""));
+    String armadilloJar = getJarFromVersion(lastVersion);
     return !fileExistsInDir(armadilloJar, armadilloHome);
   }
 
@@ -305,7 +320,7 @@ public class ManagementService {
 
     JsonElement lastRelease = getLastRelease();
     String lastVersion = getLastReleaseVersion(lastRelease);
-    String armadilloJar = String.format("molgenis-armadillo-%s.jar", lastVersion.replace("v", ""));
+    String armadilloJar = getJarFromVersion(lastVersion);
     String downloadUrl = String.format(RELEASE_DOWNLOAD_URL, lastVersion, armadilloJar);
     String armadilloInstallation = format("%s/%s", armadilloHome, armadilloJar);
 
@@ -350,29 +365,45 @@ public class ManagementService {
 
   private void downloadFile(String url, String outputFile, LongConsumer progressCallback) {
     try {
-      URLConnection connection = new URL(url).openConnection();
-      long fileSize = connection.getContentLengthLong(); // -1 if unknown
+      HttpRequest request = HttpRequest.newBuilder().uri(URI.create(url)).GET().build();
 
-      try (BufferedInputStream in = new BufferedInputStream(connection.getInputStream());
+      HttpResponse<InputStream> response =
+          HttpClient.newBuilder()
+              .proxy(ProxySelector.getDefault())
+              .followRedirects(HttpClient.Redirect.NORMAL) // follow GitHub → S3 redirect
+              .build()
+              .send(request, HttpResponse.BodyHandlers.ofInputStream());
+
+      if (response.statusCode() != 200) {
+        throw new ResponseStatusException(HttpStatusCode.valueOf(response.statusCode()));
+      }
+
+      long fileSize = response.headers().firstValueAsLong("Content-Length").orElse(-1L);
+
+      try (BufferedInputStream in = new BufferedInputStream(response.body());
           FileOutputStream fileOutputStream = new FileOutputStream(outputFile)) {
 
-        byte[] dataBuffer = new byte[1024];
+        byte[] dataBuffer = new byte[8192];
         int bytesRead;
         long totalRead = 0;
+        long lastReportedPercent = -1;
 
-        while ((bytesRead = in.read(dataBuffer, 0, 1024)) != -1) {
+        while ((bytesRead = in.read(dataBuffer, 0, dataBuffer.length)) != -1) {
           fileOutputStream.write(dataBuffer, 0, bytesRead);
           totalRead += bytesRead;
 
           if (fileSize > 0) {
             long percent = (totalRead * 100) / fileSize;
-            progressCallback.accept(percent);
+            if (percent != lastReportedPercent) {
+              lastReportedPercent = percent;
+              progressCallback.accept(percent);
+            }
           } else {
-            progressCallback.accept(totalRead); // emit bytes if size unknown
+            progressCallback.accept(totalRead);
           }
         }
       }
-    } catch (IOException e) {
+    } catch (IOException | InterruptedException e) {
       throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage());
     }
   }
