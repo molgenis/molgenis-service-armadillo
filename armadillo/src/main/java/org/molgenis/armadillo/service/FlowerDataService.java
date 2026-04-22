c5963aaa6f4af2a2f10277844ac8e54eccd79835
Tim Cadman <41470917+timcadman@users.noreply.github.com>
2026-04-22 16:38:36 +0200

    check flower superexec class prior to data push

--- armadillo/src/main/java/org/molgenis/armadillo/service/FlowerDataService.java
+++ armadillo/src/main/java/org/molgenis/armadillo/service/FlowerDataService.java
@@ -5,6 +5,8 @@ import static org.molgenis.armadillo.controller.ContainerDockerController.DOCKER
 import java.io.IOException;
 import java.io.InputStream;
 import org.molgenis.armadillo.container.FlowerDockerService;
+import org.molgenis.armadillo.container.FlowerSuperexecContainerConfig;
+import org.molgenis.armadillo.metadata.ContainerService;
 import org.molgenis.armadillo.storage.ArmadilloStorageService;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -18,15 +20,24 @@ public class FlowerDataService {
 
   private final ArmadilloStorageService storageService;
   private final FlowerDockerService flowerDockerService;
+  private final ContainerService containerService;
 
   public FlowerDataService(
-      ArmadilloStorageService storageService, FlowerDockerService flowerDockerService) {
+      ArmadilloStorageService storageService,
+      FlowerDockerService flowerDockerService,
+      ContainerService containerService) {
     this.storageService = storageService;
     this.flowerDockerService = flowerDockerService;
+    this.containerService = containerService;
   }
 
   @PreAuthorize("hasAnyRole('ROLE_SU', 'ROLE_' + #project.toUpperCase() + '_RESEARCHER')")
   public void pushData(String project, String resource, String containerName) {
+    var config = containerService.getByName(containerName);
+    if (!(config instanceof FlowerSuperexecContainerConfig)) {
+      throw new IllegalArgumentException(
+          "Container '" + containerName + "' is not a Flower clientapp container");
+    }
     String fileName = project + "_" + resource.replace("/", "_");
     try (InputStream data = storageService.loadObject(project, resource)) {
       flowerDockerService.copyDataToContainer(containerName, DATA_DIR, fileName, data);
