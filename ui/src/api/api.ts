00b50ffa2515a1c521d0499f6a279205a0c81b64
mkslofstra <mariska-karina@hotmail.com>
2026-07-21 11:04:34 +0200

    show error when cannot download updater

--- ui/src/api/api.ts
+++ ui/src/api/api.ts
@@ -110,8 +110,8 @@ export async function handleResponse(response: Response) {
         } else {
           error.message = response.statusText;
         }
-      } catch (e) {
-        error.message = response.statusText;
+      } catch (ignore) {
+        // error will be thrown with proper message
       }
     }
     throw error;
