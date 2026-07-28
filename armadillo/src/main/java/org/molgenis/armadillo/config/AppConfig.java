78f5b800da8a4e7cbcf1550b2afd49a650377218
jhhaanstra <janhendrikhaanstra@gmail.com>
2026-07-28 15:35:32 +0200

    feat: springify the PythonRebootScriptRunner and fix tests to no longer use reflection

--- armadillo/src/main/java/org/molgenis/armadillo/config/AppConfig.java
+++ armadillo/src/main/java/org/molgenis/armadillo/config/AppConfig.java
@@ -1,15 +1,41 @@
 package org.molgenis.armadillo.config;
 
+import static java.lang.String.format;
+
+import java.net.ProxySelector;
+import java.net.http.HttpClient;
 import org.molgenis.armadillo.service.PythonRebootScriptRunner;
 import org.molgenis.armadillo.service.RebootScriptRunner;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AppConfig {
 
+  private static final String DEV = "DEV";
+
   @Bean
-  RebootScriptRunner rebootScriptRunner() {
-    return new PythonRebootScriptRunner();
+  RebootScriptRunner rebootScriptRunner(
+      @Qualifier("jarHome") String jarhome,
+      @Value("${stdout.log.path:./logs/armadillo.log}") String configuredLogPath) {
+    return new PythonRebootScriptRunner(configuredLogPath, jarhome);
+  }
+
+  @Bean
+  String jarHome(
+      @Value("${armadillo.armadillo-mode:PROD}") String armadilloMode,
+      @Value("${armadillo.armadillo-home:/usr/share/armadillo/application}") String armadilloHome) {
+    if (DEV.equals(armadilloMode)) {
+      return format("%s/build/libs", armadilloHome);
+    } else {
+      return format("%s", armadilloHome);
+    }
+  }
+
+  @Bean
+  public HttpClient httpClient() {
+    return HttpClient.newBuilder().proxy(ProxySelector.getDefault()).build();
   }
 }
