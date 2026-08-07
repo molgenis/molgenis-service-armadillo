8c8586cf28f03a1cc6e1feb3cb6f1610b75073bb
mkslofstra <mariska-karina@hotmail.com>
2026-08-07 11:27:10 +0200

    fix: sonar issue

--- armadillo/src/main/java/org/molgenis/armadillo/ArmadilloServiceApplication.java
+++ armadillo/src/main/java/org/molgenis/armadillo/ArmadilloServiceApplication.java
@@ -1,37 +1,32 @@
 package org.molgenis.armadillo;
 
 import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.DefaultApplicationArguments;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.retry.annotation.EnableRetry;
 
 @SpringBootApplication(scanBasePackages = "org.molgenis")
 @EnableRetry
-public class ArmadilloServiceApplication implements ApplicationContextAware {
+public class ArmadilloServiceApplication {
   private static ConfigurableApplicationContext context;
+  private static ApplicationArguments arguments;
 
   public static void main(String[] args) {
-    SpringApplication.run(ArmadilloServiceApplication.class, args);
-  }
-
-  @Override
-  public void setApplicationContext(ApplicationContext context) {
-    ArmadilloServiceApplication.context = (ConfigurableApplicationContext) context;
+    arguments = new DefaultApplicationArguments(args);
+    context = SpringApplication.run(ArmadilloServiceApplication.class, args);
   }
 
   public static void restart() {
     // programmatically trigger restart, will not reload application.yml
-    ApplicationArguments args = context.getBean(ApplicationArguments.class);
-
     Thread thread =
         new Thread(
             () -> {
               context.close();
               context =
-                  SpringApplication.run(ArmadilloServiceApplication.class, args.getSourceArgs());
+                  SpringApplication.run(
+                      ArmadilloServiceApplication.class, arguments.getSourceArgs());
             });
 
     thread.setDaemon(false);
