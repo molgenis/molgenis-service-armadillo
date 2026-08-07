39a1d33071a612ea61f074a4e5e56e97fb60ec02
mkslofstra <mariska-karina@hotmail.com>
2026-08-07 10:52:08 +0200

    test: add missing tests

--- armadillo/src/test/java/org/molgenis/armadillo/service/PythonRebootScriptRunnerTest.java
+++ armadillo/src/test/java/org/molgenis/armadillo/service/PythonRebootScriptRunnerTest.java
@@ -4,6 +4,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.io.File;
+import java.io.IOException;
 import java.nio.file.Path;
 import java.util.concurrent.atomic.AtomicReference;
 import org.junit.jupiter.api.AfterEach;
@@ -28,6 +29,39 @@ class PythonRebootScriptRunnerTest {
   @AfterEach
   void tearDown() {
     Thread.setDefaultUncaughtExceptionHandler(null);
+  }
+
+  @Test
+  void getProcessBuilderForRebootScript() {
+    String pythonScript = "print('hello world')";
+    ProcessBuilder pb = scriptRunner.getProcessBuilderForPythonScript(pythonScript);
+    assertThat(pb.command().get(2)).isEqualTo(pythonScript);
+    assertThat(pb.redirectInput().file()).isEqualTo(new File("/dev/null"));
+  }
+
+  @Test
+  void getUpdateLogFile_createsFileIfMissing() throws IOException {
+    assertFalse(logFile.exists());
+    scriptRunner.runRebootScript();
+    assertTrue(logFile.exists());
+  }
+
+  @Test
+  void getUpdateLogFile_throwsException() {
+    scriptRunner = new RecordingPythonRebootScriptRunner("");
+    assertThrows(IOException.class, () -> scriptRunner.runRebootScript());
+  }
+
+  @Test
+  void createPythonScript_nonUpdateBranch_doesNotContainUpdateFlag() throws IOException {
+    scriptRunner.runRebootScript("armadillo-reboot.sh", "-v", "5.14.0");
+    String script = scriptRunner.getLastScriptExecution();
+
+    assertTrue(script.contains("import os, sys, subprocess"));
+    assertTrue(script.contains("with open('" + logFile.getAbsolutePath() + "', 'a') as log:"));
+    assertTrue(
+        script.contains(
+            "subprocess.run(['armadillo-reboot.sh', '-v', '5.14.0'], stdout=log, stderr=log, stdin=subprocess.DEVNULL"));
   }
 
   @Test
