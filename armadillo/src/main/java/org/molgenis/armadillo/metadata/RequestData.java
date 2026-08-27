7e92787db813090cebca28f9abadccd6a46c5344
mkslofstra <mariska-karina@hotmail.com>
2026-08-27 12:05:59 +0200

    add missing file

---
+++ armadillo/src/main/java/org/molgenis/armadillo/metadata/RequestData.java
@@ -0,0 +1,27 @@
+package org.molgenis.armadillo.metadata;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.google.auto.value.AutoValue;
+import jakarta.validation.constraints.NotEmpty;
+import java.util.ArrayList;
+
+@AutoValue
+public abstract class RequestData {
+  @JsonProperty("table")
+  @NotEmpty
+  // TODO discuss: is table sufficient? This would then need to be a path to the table, e.g.
+  // uncan/data/table.parquet
+  // alternative: specify project and folder separately
+  public abstract String getTable();
+
+  @JsonProperty("variables")
+  @NotEmpty
+  public abstract ArrayList<String> getVariables();
+
+  @JsonCreator
+  public static RequestData create(
+      @JsonProperty("table") String table, @JsonProperty("variables") ArrayList<String> variables) {
+    return new AutoValue_RequestData(table, variables);
+  }
+}
