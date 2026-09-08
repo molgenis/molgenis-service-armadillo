77927c3d277d759c8045d18674b9695dc74fae03
Tim Cadman <41470917+timcadman@users.noreply.github.com>
2026-09-08 16:18:18 +0200

    fix: only use dev BuildProperties fallback when build info is actually missing (#1078)

--- armadillo/src/main/java/org/molgenis/armadillo/config/BuildPropertiesFallbackConfig.java
+++ armadillo/src/main/java/org/molgenis/armadillo/config/BuildPropertiesFallbackConfig.java
@@ -1,13 +1,16 @@
 package org.molgenis.armadillo.config;
 
 import java.util.Properties;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.info.BuildProperties;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Condition;
+import org.springframework.context.annotation.ConditionContext;
+import org.springframework.context.annotation.Conditional;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.type.AnnotatedTypeMetadata;
 
 @Configuration
-@ConditionalOnMissingBean(BuildProperties.class)
+@Conditional(BuildPropertiesFallbackConfig.BuildInfoMissing.class)
 public class BuildPropertiesFallbackConfig {
 
   @Bean
@@ -15,5 +18,15 @@ public class BuildPropertiesFallbackConfig {
     Properties props = new Properties();
     props.setProperty("version", "dev");
     return new BuildProperties(props);
+  }
+
+  static class BuildInfoMissing implements Condition {
+    @Override
+    public boolean matches(ConditionContext context, AnnotatedTypeMetadata metadata) {
+      return !context
+          .getResourceLoader()
+          .getResource("classpath:META-INF/build-info.properties")
+          .exists();
+    }
   }
 }
